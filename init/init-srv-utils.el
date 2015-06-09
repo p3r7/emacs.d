b00f5b4b63cba66039127dc350be0127b39de042
p3r7 <contact.perf@gmail.com>
2015-06-09 17:22:00 +0200

    adding syslog-mode auto-mode trigger

--- init/init-srv-utils.el
+++ init/init-srv-utils.el
@@ -7,6 +7,7 @@
 (prf/install-package 'hide-lines)
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins-spe/syslog-mode-prf"))
 (require 'syslog-mode)
+(add-to-list 'auto-mode-alist '(".*\.log'" . syslog-mode))
 (add-to-list 'auto-mode-alist '("/var/log.*\\'" . syslog-mode))
 (add-to-list 'auto-mode-alist '("\\catalina.out\\'" . syslog-mode))
 
