963fec8041c6a796d163f2952fdee6b83624d62b
p3r7 <contact.perf@gmail.com>
2015-07-15 14:37:35 +0200

    addind back older nxhtml's version of php-mode as more stable

--- init/lang/init-php.el
+++ init/lang/init-php.el
@@ -1,7 +1,12 @@
 
+;; indentation is crappy as hell
 ;; https://github.com/ejmr/php-mode/
-(prf/require-plugin 'php-mode)
-(add-to-list 'auto-mode-alist '("\\.php?\\'" . php-mode))
+;;(prf/require-plugin 'php-mode)
+
+;(add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins-spe/nxhtml/related/php-mode.el"))
+(when (require 'php-mode nil 'noerror)
+  (add-to-list 'auto-mode-alist '("\\.php?\\'" . php-mode))
+ )
 
 ;; alternative: https://github.com/echosa/phpplus-mode
 
