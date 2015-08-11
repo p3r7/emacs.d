30d6ff968805a6a8920bcd64702130d3bb6e8473
p3r7 <contact.perf@gmail.com>
2015-08-11 14:25:55 +0200

    updated config for deft 6.0
    notably added recursive parsing and enabled support for multiple formats

--- init/components/init-deft.el
+++ init/components/init-deft.el
@@ -3,9 +3,10 @@
    deft-use-filename-as-title t
    deft-auto-save-interval 0 ;; disable autosave
    deft-directory default-directory
-   deft-extension "org"
-   deft-text-mode 'org-mode)
-  (global-set-key [f9] 'deft)
-  )
+   deft-recursive t
+   )
+
+   (global-set-key [f9] 'deft)
+   )
 
 (provide 'init-deft)
