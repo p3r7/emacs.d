959ac497a1531802e99a8e20ffe58f5a2e4a2649
p3r7 <contact.perf@gmail.com>
2015-11-05 13:51:26 +0100

    way better cygwin integration

--- init/components/init-cygwin-integration.el
+++ init/components/init-cygwin-integration.el
@@ -1,26 +1,68 @@
 ;; inspired by setup-cygwin package
 ;; not really suitable for a proper cywgin emacs install, more for using WinNT emacs alongside cygwin
 ;; TODO: also analyze this config http://stackoverflow.com/a/2078367
+;; NOTE: might also need other tweaks http://zzamboni.org/blog/making-cygwin-windows-and-emacs-understand-th/
+
+;; -------------------------------------------------------------------------
 
 (defvar cygwin-root "c:/cygwin64")
 
 (setq
- cygwin-bin (concat cygwin-root "/usr/bin")
- exec-path (cons cygwin-bin exec-path)
+ ;; cygwin-bin (concat cygwin-root "/usr/bin")
+ cygwin-bin (concat cygwin-root "/bin")
  Info-default-directory-list (append Info-default-directory-list (list cygwin-root))
  )
-;; (setenv "PATH" (concat cygwin-bin ";" (getenv "PATH")))
+
+;; trick is that :
+;; - cygwin's /usr/bin is linked to /bin, that is <cygwin-root>\bin\
+;; - cygwin's /usr/local/bin is <cygwin-root>\usr\local\bin\
+
+
+;; -------------------------------------------------------------------------
+
+;; exec-path enrichment
+
+(setenv "PATH" (concat (prf/system/get-path-system-format cygwin-bin) ";" (getenv "PATH")))
+(setq exec-path (cons cygwin-bin exec-path))
+
+
+;; -------------------------------------------------------------------------
+
+;; shells
+
 ;; (setq process-coding-system-alist '(("bash" . undecided-unix)))
-;; (setq w32-quote-process-args ?\")
+;; (setq w32-quote-process-args ?\") ;; " @@@ IS THIS BETTER? ;@@@ WAS THIS BEFORE: (setq w32-quote-process-args t)
+
+;; bash as default shell
+;; (setq shell-file-name "C:/cygwin/bin/bash.exe") ; Subprocesses invoked via the shell.
+;; (setenv "SHELL" shell-file-name)
+;; (setq explicit-shell-file-name shell-file-name) ; Interactive shell
+;; (setq ediff-shell shell-file-name)      ; Ediff shell
+;; (setq explicit-shell-args '("--login" "-i"))
+;;;;; (setq shell-command-switch "-ic") ; SHOULD THIS BE "-c" or "-ic"?
+
+
+;; -------------------------------------------------------------------------
+
+;; default coding style
 
 ;;; Use Unix-style line endings.
 ;; (setq-default buffer-file-coding-system 'undecided-unix)
+
+
+;; -------------------------------------------------------------------------
+
+;; make Emacs understand Cygwin-style path
 
 ;;; Make Cygwin paths accessible
 (prf/require-plugin 'cygwin-mount)
 (cygwin-mount-activate)
 
-;;; Follow Cygwin symlinks.
+
+;; -------------------------------------------------------------------------
+
+;; make Emacs follow Cygwin symlinks
+
 ;;; Handles old-style (text file) symlinks and new-style (.lnk file) symlinks.
 ;;; (Non-Cygwin-symlink .lnk files, such as desktop shortcuts, are still loaded as such.)
 (defun follow-cygwin-symlink ()
@@ -42,17 +84,5 @@ loaded as such.)"
             (find-alternate-file (match-string 1))))
       )))
 (add-hook 'find-file-hooks 'follow-cygwin-symlink)
-;; NOTE: might also need other tweaks http://zzamboni.org/blog/making-cygwin-windows-and-emacs-understand-th/
-
-;;; Use `bash' as the default shell in Emacs.
-;; (setq exec-path (cons "C:/cygwin/bin" exec-path))
-;; (setq shell-file-name "C:/cygwin/bin/bash.exe") ; Subprocesses invoked via the shell.
-;; (setenv "SHELL" shell-file-name)
-;; (setenv "PATH" (concat (getenv "PATH") ";C:\\cygwin\\bin"))
-;; (setq explicit-shell-file-name shell-file-name) ; Interactive shell
-;; (setq ediff-shell shell-file-name)      ; Ediff shell
-;; (setq explicit-shell-args '("--login" "-i"))
-;;;;; (setq shell-command-switch "-ic") ; SHOULD THIS BE "-c" or "-ic"?
-;; (setq w32-quote-process-args ?\") ;; " @@@ IS THIS BETTER? ;@@@ WAS THIS BEFORE: (setq w32-quote-process-args t)
 
 (provide 'init-cygwin-integration)
