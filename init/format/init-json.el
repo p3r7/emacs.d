94c55aeb65c609b8ef0a04173d752f93cffe6c08
p3r7 <contact.perf@gmail.com>
2015-11-05 13:52:30 +0100

    make json/format not necessarilly depend on python

--- init/format/init-json.el
+++ init/format/init-json.el
@@ -1,11 +1,12 @@
 
-(when (executable-find "python")
-  (defun json/format ()
-    (interactive)
-    (save-excursion
-      (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)
-      )
-    )
-  )
+(if (fboundp 'json-pretty-print)
+    (defalias 'json/format 'json-pretty-print)
+  (if (executable-find "python")
+      (defun json/format ()
+	(interactive)
+	(save-excursion
+	  (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)
+      ))))
+
 
 (provide 'init-json)
