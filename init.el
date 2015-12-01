ad9a14274bc835566cb0b5128f8d97f231d33436
p3r7 <contact.perf@gmail.com>
2015-12-01 10:29:40 +0100

    https access to melpa / marmalade

--- init.el
+++ init.el
@@ -108,8 +108,8 @@
 
 
 (when (require 'package nil 'noerror)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("melpa" . "https://melpa.milkbox.net/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
   (package-initialize)
