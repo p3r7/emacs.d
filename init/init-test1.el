d17478c8462eb165a21d3b42fd0f9ebf6de44575
p3r7 <contact.perf@gmail.com>
2016-01-26 11:24:36 +0100

    disabled garbage clx tweek

--- init/init-test1.el
+++ init/init-test1.el
@@ -23,14 +23,14 @@
 ;; make helm and lusty more snappy
 ;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
 
-(defun my-minibuffer-setup-hook ()
-  (setq gc-cons-threshold most-positive-fixnum))
+;; (defun my-minibuffer-setup-hook ()
+  ;; (setq gc-cons-threshold most-positive-fixnum))
 
-(defun my-minibuffer-exit-hook ()
-  (setq gc-cons-threshold 800000))
+;; (defun my-minibuffer-exit-hook ()
+  ;; (setq gc-cons-threshold 800000))
 
-(add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
-(add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
+;; (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
+;; (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
 
 
 
