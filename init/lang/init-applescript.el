8fc656a45c615b65b28bbab9af5e42df8856c6fe
p3r7 <wpinjp@gmail.com>
2016-01-31 16:56:35 +0100

    Correction unclosed parens

--- init/lang/init-applescript.el
+++ init/lang/init-applescript.el
@@ -1,5 +1,5 @@
 
 (when (prf/require-plugin 'applescript-mode nil 'noerror)
-  (add-to-list 'auto-mode-alist '("\\.applescript$" . applescript-mode))
+  (add-to-list 'auto-mode-alist '("\\.applescript$" . applescript-mode)))
 
 (provide 'init-applescript)
