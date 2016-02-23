d93465bf0dee17b0a78c08df19a256cd21804e7e
p3r7 <contact.perf@gmail.com>
2016-02-23 18:35:09 +0100

    ibuffer recipe rework

--- init/components/init-ibuffer.el
+++ init/components/init-ibuffer.el
@@ -1,98 +1,102 @@
 ;; TODO: customize ibuffer-formats for larger name column
 
-(setq prf/regex/remote-shell-buffer-name "^\\*\\(.*\\)@[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\\*$")
 
-(require 'ibuffer)
-(setq
- ibuffer-formats
- '((mark modified read-only " "
-	 (name 25 25 :left :elide)
-	 " "
-	 (size 9 -1 :right)
-	 " "
-	 (mode 16 16 :left :elide)
-	 " " filename-and-process)
-   (mark " "
-	 (name 16 -1)
-	 " " filename))
+(when (prf/require-plugin 'ibuffer nil 'noerror)
 
- ibuffer-expert t
- ibuffer-show-empty-filter-groups nil
- ibuffer-saved-filter-groups
- (quote (("default"
-	  ("shells"
-	   (or (mode . shell-mode)
-	       (name . "^\\*\\(.*\\)@[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\\*\\(.*\\)$") )
-	   )
-	  ("dired"
-	   (mode . dired-mode)
-	   )
-	  ("org" ;; all org-related buffers
-	   (or
-	    (name . "^\\*Deft\\*$")
-	    (mode . org-mode))
-	   )
-	  ;; ("Mail"
-	  ;;   (or
-	  ;;    (mode . message-mode)
-	  ;;    (mode . mail-mode)
-	  ;;    ))
+  (setq
+   ibuffer-formats
+   '((mark modified read-only " "
+	   (name 25 25 :left :elide)
+	   " "
+	   (size 9 -1 :right)
+	   " "
+	   (mode 16 16 :left :elide)
+	   " " filename-and-process)
+     (mark " "
+	   (name 16 -1)
+	   " " filename))
 
-	  ("config"
-	   (or (filename . ".emacs.d")
-	       (filename . "AutoHotkey.ahk")
-	       )
-	   )
-	  ("XSLT"
-	   (filename . "\\.xsl$")
-	   )
-	  ;; ("MyProject1"
-	  ;;   (filename . "src/myproject1/"))
-	  ("provi"
-	   (filename . "^provi")
-	   )
-	  ("dev"
-	   (or
-	    (mode . c-mode)
-	    (mode . java-mode)
-	    (mode . groovy-mode)
-	    (mode . web-mode)
-	    (mode . javascript-mode)
-	    (mode . js-mode)
-	    (mode . js3-mode)
-	    (mode . css-mode)
-	    (mode . php-mode)
-	    (mode . shell-script-mode)
-	    (mode . perl-mode)
-	    (mode . python-mode)
-	    (mode . emacs-lisp-mode)
-	    ))
-	  ("temp"
-	   (or
-	    (name . "^\\*tramp")
-	    (name . "^\\*Apropos\\*$")
-	    (name . "^\\*Messages\\*$")
-	    (name . "^\\*Completions\\*$")
-	    (name . "^\\*Help\\*$")
-	    ))
-	  ;; ("ERC"   (mode . erc-mode))
-	  ))))
+   ibuffer-expert t
+   ibuffer-show-empty-filter-groups nil
+   ibuffer-saved-filter-groups
+   (quote (("default"
+	    ("shells"
+	     (or (mode . shell-mode)
+		 (name . "^\\*\\(.*\\)@[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\.[1-9][0-9]?[0-9]?\\*\\(.*\\)$") )
+	     )
+	    ("dired"
+	     (mode . dired-mode)
+	     )
+	    ("org" ;; all org-related buffers
+	     (or
+	      (name . "^\\*Deft\\*$")
+	      (mode . org-mode))
+	     )
+	    ;; ("Mail"
+	    ;;   (or
+	    ;;    (mode . message-mode)
+	    ;;    (mode . mail-mode)
+	    ;;    ))
 
-(require 'ibuf-ext)
-(add-to-list 'ibuffer-never-show-predicates "^\\*tramp")
-(add-to-list 'ibuffer-never-show-predicates "^\\*helm")
-(add-to-list 'ibuffer-never-show-predicates "^\\.projectile")
-(add-to-list 'ibuffer-never-show-predicates "^\\*Backtrace\\$*")
-(add-to-list 'ibuffer-never-show-predicates "^\\*About GNU Emacs\\*$")
-(add-to-list 'ibuffer-never-show-predicates "^\\*Bookmark List\\*$")
+	    ("config"
+	     (or (filename . ".emacs.d")
+		 (filename . "AutoHotkey.ahk")
+		 )
+	     )
+	    ("XSLT"
+	     (filename . "\\.xsl$")
+	     )
+	    ;; ("MyProject1"
+	    ;;   (filename . "src/myproject1/"))
+	    ("provi"
+	     (filename . "^provi")
+	     )
+	    ("dev"
+	     (or
+	      (mode . c-mode)
+	      (mode . java-mode)
+	      (mode . groovy-mode)
+	      (mode . web-mode)
+	      (mode . javascript-mode)
+	      (mode . js-mode)
+	      (mode . js3-mode)
+	      (mode . css-mode)
+	      (mode . php-mode)
+	      (mode . shell-script-mode)
+	      (mode . perl-mode)
+	      (mode . python-mode)
+	      (mode . emacs-lisp-mode)
+	      ))
+	    ("temp"
+	     (or
+	      (name . "^\\*tramp")
+	      (name . "^\\*magit:")
+	      (name . "^\\*Apropos\\*$")
+	      (name . "^\\*Messages\\*$")
+	      (name . "^\\*Completions\\*$")
+	      (name . "^\\*Help\\*$")
+	      ))
+	    ;; ("ERC"   (mode . erc-mode))
+	    ))))
 
-(add-hook 'ibuffer-mode-hook
-	  (lambda ()
-	    (ibuffer-auto-mode 1)
-	    (ibuffer-switch-to-saved-filter-groups "default")))
+  (require 'ibuf-ext)
+  (add-to-list 'ibuffer-never-show-predicates "^\\*tramp")
+  (add-to-list 'ibuffer-never-show-predicates "^\\*magit:")
+  (add-to-list 'ibuffer-never-show-predicates "^\\*helm")
+  (add-to-list 'ibuffer-never-show-predicates "^\\.projectile")
+  (add-to-list 'ibuffer-never-show-predicates "^\\*Backtrace\\$*")
+  (add-to-list 'ibuffer-never-show-predicates "^\\*About GNU Emacs\\*$")
+  (add-to-list 'ibuffer-never-show-predicates "^\\*Bookmark List\\*$")
 
-(global-set-key (kbd "C-x B") 'ibuffer)
+  (add-hook 'ibuffer-mode-hook
+	    (lambda ()
+	      (ibuffer-auto-mode 1)
+	      (ibuffer-switch-to-saved-filter-groups "default")))
+
+  (global-set-key (kbd "C-x B") 'ibuffer)
+  )
 
 ;; TODO: https://github.com/svend/ibuffer-tramp/blob/master/ibuffer-tramp.el
+;; http://www.emacswiki.org/emacs/IbufferMode
 
 (provide 'init-ibuffer)
