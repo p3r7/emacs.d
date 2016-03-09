f7ceaa94167b6ef69caa8515dd587d11a762998a
p3r7 <contact.perf@gmail.com>
2016-03-09 18:43:22 +0100

    using more modern ahk-mode

--- init/conf/init-ahk.el
+++ init/conf/init-ahk.el
@@ -1,10 +1,19 @@
-;; [AutoHotKey]
+
 ;; http://www.emacswiki.org/emacs/AutoHotKey_Mode
-;; http://ergoemacs.googlecode.com/svn/trunk/packages/xahk-mode.el
 ;; (setq ahk-syntax-directory "~/.emacs.d/plugins/ahk-mode/Syntax")
 ;; (add-to-list 'auto-mode-alist '("\\.ahk$" . ahk-mode))
 ;; (autoload 'ahk-mode "ahk-mode")
-(when (prf/require-plugin 'xahk-mode "~/.emacs.d/plugins/xahk-mode" 't)
-  (add-to-list 'auto-mode-alist '("\\.ahk$" . xahk-mode))
-  )
-(provide 'init-ahk)
+
+;; - [] http://ergoemacs.googlecode.com/svn/trunk/packages/xahk-mode.el
+;; (when (prf/require-plugin 'xahk-mode "~/.emacs.d/plugins/xahk-mode" 't)
+;; (add-to-list 'auto-mode-alist '("\\.ahk$" . xahk-mode)))
+
+;; - [X] https://github.com/ralesi/ahk-mode
+(when (prf/require-plugin 'ahk-mode nil 'noerror)
+  (add-hook 'ahk-mode-hook
+	    (lambda()
+	      (local-set-key (kbd "C-c C-e") 'ahk-run-script))))
+;; (when (prf/require-plugin 'ahk-mode noerror 'noerror)
+;; (add-to-list 'auto-mode-alist '("\\.ahk$" . ahk-mode)))
+
+  (provide 'init-ahk)
