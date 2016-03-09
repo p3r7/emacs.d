be8e94ad3fdb4ea586891cc9de99d698fdeee9df
p3r7 <contact.perf@gmail.com>
2016-03-09 18:43:41 +0100

    Using more modern php-mode

--- init/lang/init-php.el
+++ init/lang/init-php.el
@@ -3,10 +3,11 @@
 ;; https://github.com/ejmr/php-mode/
 ;;(prf/require-plugin 'php-mode)
 
-;(add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins-spe/nxhtml/related/php-mode.el"))
-(when (require 'php-mode nil 'noerror)
+;;(add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins-spe/nxhtml/related/php-mode.el"))
+;; (when (prf/require-plugin-from-file php-mode "~/.emacs.d/plugins-spe/nxhtml/related" nil 'noerror)
+(when (prf/require-plugin 'php-mode nil 'noerror)
   (add-to-list 'auto-mode-alist '("\\.php?\\'" . php-mode))
- )
+  )
 
 ;; alternative: https://github.com/echosa/phpplus-mode
 
