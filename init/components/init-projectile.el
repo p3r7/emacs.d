429620856cb5c0e75a3420497b61abec1d630912
U-HQ\jordan.besly <jordan.besly@wks128.hq.transatel.com>
2016-03-22 18:39:50 +0100

    Prevent Projectile from misbehaving w/ TRAMP

--- init/components/init-projectile.el
+++ init/components/init-projectile.el
@@ -7,18 +7,18 @@
   (setq projectile-file-exists-remote-cache-expire nil)
   ;; https://www.reddit.com/r/emacs/comments/320cvb/projectile_slows_tramp_mode_to_a_crawl_is_there_a/
   ;; (add-hook 'find-file-hook
-	    ;; (lambda ()
-	      ;; (when (file-remote-p default-directory)
-		;; (setq-local projectile-mode-line "Projectile"))))
+  ;; (lambda ()
+  ;; (when (file-remote-p default-directory)
+  ;; (setq-local projectile-mode-line "Projectile"))))
   ;; or
-  ;; (defadvice projectile-project-root (around ignore-remote first activate)
-    ;; (unless (file-remote-p default-directory) ad-do-it))
+  (defadvice projectile-project-root (around ignore-remote first activate)
+    (unless (file-remote-p default-directory) ad-do-it))
 
   (projectile-global-mode)
 
   ;; (if (featurep 'grizzl)
-      ;; (setq projectile-completion-system 'grizzl)
-    ;; )
+  ;; (setq projectile-completion-system 'grizzl)
+  ;; )
 
   ;; ----------------------------------------------------------------------
   ;; HELM PROJECTILE
