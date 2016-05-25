7764c27a38f6ecaf2dea716d85e49b2c73955ca4
jordan <jordan.besly@transatel.com>
2016-05-25 18:23:13 +0200

    Corrected min-version condition for hydra

--- init/components/init-hydra.el
+++ init/components/init-hydra.el
@@ -1,6 +1,6 @@
-(when (and (>= emacs-major-version 24)
-	   (>= emacs-minor-version 4))
-  (prf/require-plugin 'hydra nil 'noerror)
-  )
+(when (or (> emacs-major-version 24)
+	  (or (= emacs-major-version 24)
+	      (>= emacs-minor-version 4)))
+  (prf/require-plugin 'hydra nil 'noerror))
 
 (provide 'init-hydra)
