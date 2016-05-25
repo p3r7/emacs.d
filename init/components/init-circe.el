2b66bfbdad6e1f442d79dedd2c1894fe1b007850
jordan <jordan.besly@transatel.com>
2016-05-25 18:22:10 +0200

    Moved IRC cnnx conf to private .el

--- init/components/init-circe.el
+++ init/components/init-circe.el
@@ -1,10 +1,5 @@
 
 (prf/require-plugin 'circe)
 
-(setq circe-network-options
-      `(("Freenode"
-         :nick "p3r7"
-         :channels ("#emacs" "#emacs-circe")
-         :nickserv-password ,freenode-password)))
 
 (provide 'init-circe)
