9bcb622581d872c0adb383461d0f133b052fcaaf
jordan <jordan.besly@transatel.com>
2016-05-31 13:51:17 +0200

    Changed implementation of code folding

---
+++ init/components/init-hideshowvis.el
@@ -0,0 +1,22 @@
+
+;; [[http://www.emacswiki.org/emacs/HideShow]]
+;; https://github.com/shanecelis/hideshow-org  http://gnufool.blogspot.fr/2009/03/make-hideshow-behave-more-like-org-mode.html
+;; https://gist.github.com/doitian/1571162
+;; (require 'hideshow-fringe)
+
+(when (prf/require-plugin 'hideshowvis nil 'noerror)
+  (setq hideshowvis-ignore-same-line t) ;; breaks stuff
+  (hideshowvis-symbols) ;; same code as hideshow-fringe, but kinda breaks stuff
+
+  (defun prf/hideshowvis-c-mode-common-hook ()
+    (hs-minor-mode t)
+    (hideshowvis-minor-mode t)
+    (local-set-key (kbd "C-c <right>") 'hs-show-block)
+    (local-set-key (kbd "C-c <left>")  'hs-hide-block)
+    (local-set-key (kbd "C-c <up>")    'hs-hide-all)
+    (local-set-key (kbd "C-c <down>")  'hs-show-all))
+
+  (add-hook 'c-mode-common-hook 'prf/origami-prog-mode-hook))
+
+
+(provide 'init-hideshowvis)
---
+++ init/components/init-origami.el
@@ -0,0 +1,13 @@
+
+(when (prf/require-plugin 'origami nil 'noerror)
+
+  (defun prf/origami-prog-mode-hook ()
+    (origami-mode)
+    (local-set-key (kbd "C-c <right>") 'origami-show-node)
+    (local-set-key (kbd "C-c <left>")  'origami-close-node)
+    (local-set-key (kbd "C-c <up>")    'origami-close-all-nodes)
+    (local-set-key (kbd "C-c <down>")  'origami-open-all-nodes))
+
+  (add-hook 'prog-mode-hook 'prf/origami-prog-mode-hook))
+
+(provide 'init-origami)
