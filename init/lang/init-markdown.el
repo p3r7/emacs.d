9932bb46fbab29e704ec2e66596b676b8509730e
perf <contact.perf@gmail.com>
2017-10-03 15:19:09 +0200

    Added Pandoc as HTML markdown exporter

--- init/lang/init-markdown.el
+++ init/lang/init-markdown.el
@@ -2,6 +2,8 @@
 (when (prf/require-plugin 'markdown-mode nil 'noerror)
   (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-)
+  (when (executable-find "pandoc")
+    (setq markdown-command "pandoc -t html5 --mathjax --highlight-style pygments --standalone"))
+  )
 
 (provide 'init-markdown)
