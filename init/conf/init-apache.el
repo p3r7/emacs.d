a693b28cfb77f915a3688fbe810643b6fac0a5e3
jordan.besly <jordan.besly@transatelcom>
2018-05-11 10:44:58 +0200

    Corrected require statement

--- init/conf/init-apache.el
+++ init/conf/init-apache.el
@@ -1,9 +1,10 @@
 
-(autoload 'apache-mode "apache-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
-(add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
-(add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
-(add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
-(add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode))
+(when (prf/require-plugin 'apache-mode nil 'noerror)
+  (autoload 'apache-mode "apache-mode" nil t)
+  (add-to-list 'auto-mode-alist '("\\.htaccess\\'"   . apache-mode))
+  (add-to-list 'auto-mode-alist '("httpd\\.conf\\'"  . apache-mode))
+  (add-to-list 'auto-mode-alist '("srm\\.conf\\'"    . apache-mode))
+  (add-to-list 'auto-mode-alist '("access\\.conf\\'" . apache-mode))
+  (add-to-list 'auto-mode-alist '("sites-\\(available\\|enabled\\)/" . apache-mode)))
 
 (provide 'init-apache)
