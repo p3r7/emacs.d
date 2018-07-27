871eb9404d1f6f5ff408b043348921ada553fa07
jordan.besly <jordan.besly@transatelcom>
2018-07-27 13:34:20 +0200

    Comments about attempt to fix recent php-mode bugs

--- init/lang/init-php.el
+++ init/lang/init-php.el
@@ -1,12 +1,24 @@
+;; We use an old ass version of php-mode bundled inside nxhtml-mode.
+;; The reason is that modern php-mode versions have crappy as hell indentation.
+;; Even when forcing it back w/ prf/indentation-php-mode-hook, it craps with
+;; functions outside of classes.
 
-;; indentation is crappy as hell
-;; https://github.com/ejmr/php-mode/
-;;(prf/require-plugin 'php-mode)
+(defun prf/indentation-php-mode-hook ()
+  ;; php-mode identation is rather esotheric
+  ;; it misinterprets c-common rules
+  ;; it interprets indent-tabs-mode as indenting w/ spaces...
+  (setq tab-width 4
+	indent-tabs-mode t)
+  (c-set-style "symfony2")
+  )
 
-;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins-spe/nxhtml/related/php-mode.el"))
+
 (when (prf/require-plugin-from-file 'php-mode "~/.emacs.d/plugins-spe/nxhtml/related" 'noerror)
   ;; (when (prf/require-plugin 'php-mode nil 'noerror)
   (add-to-list 'auto-mode-alist '("\\.php?\\'" . php-mode))
+
+
+  ;; (add-hook 'php-mode-hook 'prf/indentation-php-mode-hook)
   )
 
 ;; alternative: https://github.com/echosa/phpplus-mode
