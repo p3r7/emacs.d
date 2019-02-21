a4bc05868863550fdf8d82e752b0fce47df0b985
p3r7 <contact.perf@gmail.com>
2019-02-21 21:27:51 +0100

    Changes to markdown / shiatsu notes minor mode

--- init/lang/init-md-org-shiatsu.el
+++ init/lang/init-md-org-shiatsu.el
@@ -4,6 +4,8 @@
 
   (defun prf/md-hook/enable-md-shiatsu-minor-mode ()
     (when (string-match-p (regexp-quote "shiatsu") default-directory)
+      (when (executable-find "multimarkdown")
+	(setq markdown-command (executable-find "multimarkdown")))
       (prf-md-shiatsu 1)))
 
   (add-hook 'markdown-mode-hook 'prf/md-hook/enable-md-shiatsu-minor-mode))
