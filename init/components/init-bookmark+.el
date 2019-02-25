c7ee1c3066ced56cc1b935c7b556ce2f0ff38bc2
perf <11557146+p3r7@users.noreply.github.com>
2019-02-25 11:05:29 +0100

    Fallbacking to old require method, fails w/ use-pacakge

--- init/components/init-bookmark+.el
+++ init/components/init-bookmark+.el
@@ -1,17 +1,25 @@
 
 (setq Buffer-menu-name-width 40)
 
-(use-package bookmark+
-  :defer t
-  :load-path "~/.emacs.d/plugins/bookmark+"
-  :bind (("<f10>" . list-bookmarks)
-	 ("C-<f10>" . bookmark-set))
-  :init
+
+(when (prf/require-plugin 'bookmark+ nil 'noerror)
+  (global-set-key [f10] 'list-bookmarks)
+  (global-set-key (kbd "C-<f10>") 'bookmark-set)
+  (add-to-list 'display-buffer-alist `(,(concat "^\\*Bookmark List\\*$") display-buffer-same-window))
   (setq
    bookmark-default-file "~/.emacs.d/bookmarks" ;; keep my ~/ clean
    bookmark-save-flag 1)                        ;; autosave each change
-  ;; don't create new buffer
-  (add-to-list 'display-buffer-alist `(,(concat "^\\*Bookmark List\\*$") display-buffer-same-window)))
+  )
+
+;; (use-package bookmark+
+;;   :load-path "~/.emacs.d/plugins/bookmark+"
+;;   :bind (("<f10>" . list-bookmarks)
+;; 	 ("C-<f10>" . bookmark-set))
+;;   :init
+;;   (setq
+;;    bookmark-default-file "~/.emacs.d/bookmarks" ;; keep my ~/ clean
+;;    bookmark-save-flag 1)                        ;; autosave each change
+;;   (add-to-list 'display-buffer-alist `(,(concat "^\\*Bookmark List\\*$") display-buffer-same-window)))
 
 (use-package list-register
   :bind ("C-x r l" . list-register))
