239910e5ee47a5b433d54b99f489c34d720d9a1e
perf <11557146+p3r7@users.noreply.github.com>
2019-03-07 15:54:55 +0100

    Added goto-line-preview

--- init/init-buffer-navigation.el
+++ init/init-buffer-navigation.el
@@ -4,5 +4,8 @@
 (use-package goto-last-change
   :bind ([(meta p)(u)] . goto-last-change))
 
+(use-package goto-line-preview
+  :bind ([remap goto-line] . goto-line-preview))
+
 
 (provide 'init-buffer-navigation)
