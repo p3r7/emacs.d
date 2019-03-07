fbd851c53c0f55e3b2a67ecf748c6fd2aadd5e8a
perf <11557146+p3r7@users.noreply.github.com>
2019-03-07 15:52:44 +0100

    Added helpful

--- init/init-doc.el
+++ init/init-doc.el
@@ -1,5 +1,15 @@
 
 ;; -------------------------------------------------------------------------
+;; HELP / DESCRIBE
+
+(use-package helpful
+  :bind (("C-h f" . helpful-callable)
+	 ("C-h v" . helpful-variable)
+	 ("C-h k" . helpful-key)
+	 ("C-h o" . helpful-at-point)
+	 ("C-h F" . helpful-function)))
+
+;; -------------------------------------------------------------------------
 ;; INFO
 
 ;; TODO: http://ergoemacs.org/emacs/emacs_adding_browser_keys.html
