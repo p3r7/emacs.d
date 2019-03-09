a9bd2fb2a67aeff8bb44f5ee65cbeaf5d8fea7cb
perf <11557146+p3r7@users.noreply.github.com>
2019-03-09 13:09:12 +0100

    use-package applied to org config

--- init/lang/init-org.el
+++ init/lang/init-org.el
@@ -1,19 +1,100 @@
 ;;TODO: [[http://orgmode.org/worg/org-faq.html#load-org-after-setting-variables]]
 
-(require 'org-install)
-(require 'org-habit)
+(use-package org
+  :init
 
-;; ------------------------------------------------------------------------
-;; BASIC
+  ;; GENERAL
+  (setq
+   org-directory default-directory)
 
-(setq
- org-directory default-directory
- ;; org-hide-leading-stars t
- org-yank-adjusted-subtrees t ;; adjust level while pasting, if not wanted do C-u C-y
- ;; org-default-notes-file (concat org-directory "/captures.org")
- ;; org-agenda-files (list (concat org-directory "/tasks.org")
- ;; (concat org-directory "/captures.org"))
- )
+  ;; DISPLAY
+  (setq
+   ;; org-hide-leading-stars t
+   org-ellipsis "⤵"
+   org-highlight-latex-and-related '(latex))
+
+  ;; EDITING BEAHVIOURS
+  (setq
+   org-yank-adjusted-subtrees t ;; adjust level while pasting, if not wanted do C-u C-y
+   )
+
+  ;; CAPTURE
+  ;; (setq org-default-notes-file (concat org-directory "/captures.org")
+  ;; 	org-agenda-files (list (concat org-directory "/tasks.org")
+  ;; 			       (concat org-directory "/captures.org")))
+
+  ;; CAPTURE: MobileOrg
+  (setq org-mobile-inbox-for-pull "~/Dropbox/textfiles/mobileorg.org"
+	org-mobile-directory "~/Dropbox/textfiles/MobileOrg")
+
+  ;; EXPORT
+  (setq
+   org-export-with-section-numbers nil
+   org-html-validation-link nil)
+
+  ;; LINKS
+  ;; http://orgmode.org/manual/Code-evaluation-security.html
+  ;; Safe as long as I only use my own org files
+  ;; (require 'org-link-sshell)
+  (setq org-confirm-shell-link-function nil
+	org-confirm-elisp-link-function nil)
+  ;; (setq org-link-abbrev-alist
+  ;;       '(
+  ;;         ("google"   . "http://www.google.com/search?q=")
+  ;;         ("gmap"     . "http://maps.google.com/maps?q=%s")
+  ;;         ))
+
+  :config
+  (require 'org-install)
+  (require 'org-habit)
+
+  ;; DISPLAY
+  (org-display-inline-images t t)
+
+  ;; BABEL
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((C . t)))
+
+  ;; TIME TRACKING
+  ;; [[http://orgmode.org/manual/Clocking-work-time.html]]
+  (setq org-clock-persist 'history)
+  (org-clock-persistence-insinuate)
+  (setq org-clock-persist t)
+
+  ;; WYSIWYG
+  (defvar prf/org/wysiwyg-active nil)
+  (defvar prf/org/wysiwyg-vars-default-alist nil)
+  (defvar prf/org/wysiwyg-vars-override-alist
+    '((org-startup-indented . t)
+      (org-bullets-bullet-list . '(" "))
+      (org-pretty-entities . t)
+      (org-hide-emphasis-markers . t)
+      (org-agenda-block-separator . "")
+      (org-fontify-whole-heading-line . t)
+      (org-fontify-done-headline . t)
+      (org-fontify-quote-and-verse-blocks . t)))
+
+
+  ;; (defun prf/org/toggle-wysiwyg ()
+  ;;   (interactive)
+
+  ;;   (if prf/org/wysiwyg-active
+  ;; 	(progn ())
+  ;;     (setq prf/org/wysiwyg-vars-default-alist
+  ;; 	    (--map (cons (car it) (eval (car it))) prf/org/wysiwyg-vars-override-alist))
+  ;;     (setq org-startup-indented t
+  ;; 	    org-bullets-bullet-list '(" ")
+  ;; 	    org-pretty-entities t
+  ;; 	    org-hide-emphasis-markers t
+  ;; 	    org-agenda-block-separator ""
+  ;; 	    org-fontify-whole-heading-line t
+  ;; 	    org-fontify-done-headline t
+  ;; 	    org-fontify-quote-and-verse-blocks
+  ;; 	    )
+  ;;     (setq prf/org/wysiwyg-active t)))
+
+  )
 
 
 ;; ------------------------------------------------------------------------
@@ -21,11 +102,6 @@
 
 ;; http://stackoverflow.com/questions/17621495/emacs-org-display-inline-images
 (org-display-inline-images t t)
-
-(setq
- org-ellipsis "⤵"
- org-highlight-latex-and-related '(latex)
- )
 
 
 ;; ------------------------------------------------------------------------
@@ -36,10 +112,8 @@
 (use-package htmlize
   :after (org))
 
-(setq org-export-with-section-numbers nil)
-(setq org-html-validation-link nil)
-
-;; (prf/require-plugin 'ox-slack)
+(use-package ox-slack
+  :after (org))
 
 
 ;; ------------------------------------------------------------------------
@@ -49,51 +123,6 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ci" 'org-insert-link)
 (global-set-key "\C-ca" 'org-agenda)
-
-
-;; ------------------------------------------------------------------------
-;; TIME TRACKING
-
-;; [[http://orgmode.org/manual/Clocking-work-time.html]]
-;; http://orgmode.org/manual/Clocking-work-time.html#fnd-2
-(setq org-clock-persist 'history)
-(org-clock-persistence-insinuate)
-(setq org-clock-persist t)
-
-
-;; ------------------------------------------------------------------------
-;; BABEL
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((C . t)
-   ))
-
-
-;; ------------------------------------------------------------------------
-;; LINKS
-
-;; (setq org-link-abbrev-alist
-;;       '(
-;;         ("google"   . "http://www.google.com/search?q=")
-;;         ("gmap"     . "http://maps.google.com/maps?q=%s")
-;;         ))
-
-;; http://orgmode.org/manual/Code-evaluation-security.html
-;; Safe as long as I only use my own org files
-;; (require 'org-link-sshell)
-(setq org-confirm-shell-link-function nil
-      org-confirm-elisp-link-function nil)
-
-
-;; ------------------------------------------------------------------------
-;; MOBILE ORG
-
-;; MobileOrg
-;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull "~/Dropbox/textfiles/mobileorg.org")
-;; Set to <your Dropbox root directory>/MobileOrg.
-(setq org-mobile-directory "~/Dropbox/textfiles/MobileOrg")
 
 
 ;; ------------------------------------------------------------------------
