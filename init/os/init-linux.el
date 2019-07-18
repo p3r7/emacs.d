7a6975002c344694f1cb5565542471ea2aa87bf4
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-07-18 06:42:53 +0200

    Made older toggle-frame-fullscreen deprecated

--- init/os/init-linux.el
+++ init/os/init-linux.el
@@ -10,19 +10,18 @@
 
 
 ;; ------------------------------------------------------------------------
-;; FULLSCREEN
+;; FULLSCREEN (OLDER EMACS)
 
 ;; NOTE: does not work for some WM (e.g. xmonad)
 ;; BUG: tries to execute it on remote srv if tramp cnnx
-(when (executable-find "wmctrl")
+(when (and (not (fboundp 'toggle-frame-fullscreen))  (executable-find "wmctrl"))
   (defun prf/toggle-fullscreen-wmctrl ()
     "toggle full-screen mode"
     (interactive)
     (progn
-      (shell-command "wmctrl -r :ACTIVE: -btoggle,fullscreen")
-      ))
+      (shell-command "wmctrl -r :ACTIVE: -btoggle,fullscreen")))
   (global-set-key (kbd "<f11>")  'prf/toggle-fullscreen-wmctrl)
-  )
+  (defalias '_sh 'toggle-frame-fullscreen))
 
 
 ;; ------------------------------------------------------------------------
