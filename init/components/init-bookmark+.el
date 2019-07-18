a265587e3ab7a6d3be449d6acf13871422f7a363
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-07-18 06:38:33 +0200

    Newer bookmark+ for systems supporting Quelpa

--- init/components/init-bookmark+.el
+++ init/components/init-bookmark+.el
@@ -1,15 +1,25 @@
 
 (setq Buffer-menu-name-width 40)
 
+(if (windows-nt-p)
+    (when (prf/require-plugin 'bookmark+ nil 'noerror)
+      (global-set-key [f10] 'list-bookmarks)
+      (global-set-key (kbd "C-<f10>") 'bookmark-set)
+      (add-to-list 'display-buffer-alist `(,(concat "^\\*Bookmark List\\*$") display-buffer-same-window))
+      (setq
+       bookmark-default-file "~/.emacs.d/bookmarks" ;; keep my ~/ clean
+       bookmark-save-flag 1))                        ;; autosave each change
+  (use-package bookmark+
+    :quelpa (bookmark+ :fetcher github :repo "emacsmirror/bookmark-plus")
+    :bind (("<f10>" . list-bookmarks)
+	   ("C-<f10>" . bookmark-set))
+    :init
+    (setq
+     bookmark-default-file "~/.emacs.d/bookmarks" ;; keep my ~/ clean
+     bookmark-save-flag 1)                        ;; autosave each change
+    :config
+    (add-to-list 'display-buffer-alist `(,(concat "^\\*Bookmark List\\*$") display-buffer-same-window))))
 
-(when (prf/require-plugin 'bookmark+ nil 'noerror)
-  (global-set-key [f10] 'list-bookmarks)
-  (global-set-key (kbd "C-<f10>") 'bookmark-set)
-  (add-to-list 'display-buffer-alist `(,(concat "^\\*Bookmark List\\*$") display-buffer-same-window))
-  (setq
-   bookmark-default-file "~/.emacs.d/bookmarks" ;; keep my ~/ clean
-   bookmark-save-flag 1)                        ;; autosave each change
-  )
 
 ;; (use-package bookmark+
 ;;   ;; :load-path "~/.emacs.d/plugins/bookmark+"
