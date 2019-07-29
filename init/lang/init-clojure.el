1e623495d9f0c51841b2e8cdb8b58a3e586cf4b6
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-07-29 22:17:30 +0200

    Replace kill-buffer w/ cider-quit for CIDER REPL

--- init/lang/init-clojure.el
+++ init/lang/init-clojure.el
@@ -1,7 +1,9 @@
 
 (use-package clojure-mode)
 (use-package cider
+  :bind (:map cider-repl-mode-map
+	      ("C-x k" . cider-quit)
+	      ("C-x C-k" . cider-quit))
   :after (clojure-mode))
-
 
 (provide 'init-clojure)
