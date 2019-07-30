e38047cb486fa73422bac6bc36e3bb9d1e312696
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-07-31 00:07:13 +0200

    Enabling clojure in org-babel

--- init/lang/init-clojure.el
+++ init/lang/init-clojure.el
@@ -4,6 +4,13 @@
   :bind (:map cider-repl-mode-map
 	      ("C-x k" . cider-quit)
 	      ("C-x C-k" . cider-quit))
-  :after (clojure-mode))
+  :after (clojure-mode)
+  :init
+  (setq org-babel-clojure-backend 'cider)
+  :config
+  (require 'ob-clojure))
+
+(use-package cider-eval-sexp-fu
+  :after (cider))
 
 (provide 'init-clojure)
