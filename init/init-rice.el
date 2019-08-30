e79794c6daf13652e20acc1c67348e7a0f6a8c54
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-08-30 13:14:48 +0200

    CHanged shortcut to AZERTY / ISO keyboard layout

--- init/init-rice.el
+++ init/init-rice.el
@@ -139,7 +139,7 @@
 ;; increase opacity (== decrease transparency) => (djcb-opacity-modify)
 (global-set-key (kbd "M-°") '(lambda()(interactive)(djcb-opacity-modify)))
 ;; decrease opacity (== increase transparency => (djcb-opacity-modify t)
-(global-set-key (kbd "M-+") '(lambda()(interactive)(djcb-opacity-modify t)))
+(global-set-key (kbd "C-M-+") '(lambda()(interactive)(djcb-opacity-modify t)))
 ;; return the state to normal
 (global-set-key (kbd "M-=") '(lambda()(interactive)
                                (modify-frame-parameters nil `((alpha . 100)))))
