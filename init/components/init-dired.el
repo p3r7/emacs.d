87075134bf501ec890ad9fd6f717dd3bf0b6b39a
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-08-30 13:13:12 +0200

    base dired conf as use-package block + new shortcuts

--- init/components/init-dired.el
+++ init/components/init-dired.el
@@ -7,31 +7,45 @@
 ;; could use https://emacs.stackexchange.com/questions/35676/customize-direds-display
 ;; or get inspired by dired-hide-details-mode
 
-(setq
- dired-dwim-target t  ;; if other window -> set as default dir for copy
- ls-lisp-dirs-first t ;; display dirs 1st
- dired-listing-switches "-alh"
- ;; dired-listing-switches "-alDphgG"
- diredp-hide-details-initially-flag nil
- diredp-hide-details-propagate-flag nil)
 
-(when (executable-find "busybox")
-  (setq dired-use-ls-dired nil))
+(use-package dired
+  :ensure nil
+  :demand
+  :bind (:map dired-mode-map
+	      ("C-c C" . prf/dired-do-copy-not-dwin)
+	      ("C-c R" . prf/dired-do-rename-not-dwin)
+	      ;; do not create other dired buffers when navigating
+	      ;; TODO: far from being perfect (closes all dired windows, not just current)
+	      ("<return>" . dired-find-alternate-file)
+	      ("^" . (lambda () (interactive) (find-alternate-file ".."))))
+  :init
+  (setq
+   dired-dwim-target t	;; if other window -> set as default dir for copy
+   ls-lisp-dirs-first t ;; display dirs 1st
+   dired-listing-switches "-alh"
+   ;; dired-listing-switches "-alDphgG"
+   diredp-hide-details-initially-flag nil
+   diredp-hide-details-propagate-flag nil)
 
-(put 'dired-find-alternate-file 'disabled nil)
+  (when (executable-find "busybox")
+    (setq dired-use-ls-dired nil))
 
-(add-hook 'dired-mode-hook ;; do not create other dired buffers when navigating
-	  ;; TODO: far from being perfect (closes all dired windows, not just current)
-	  (lambda ()
-	    (define-key dired-mode-map (kbd "<return>")
-	      #'dired-find-alternate-file) ; was dired-advertised-find-file
-	    (define-key dired-mode-map (kbd "^")
-	      (lambda () (interactive) (find-alternate-file "..")))
-					; was dired-up-directory
-	    ))
+  :config
+  (defun prf/dired-do-copy-not-dwin ()
+    (interactive)
+    (let ((dired-dwim-target nil))
+      (dired-do-copy)))
+  (defun prf/dired-do-rename-not-dwin ()
+    (interactive)
+    (let ((dired-dwim-target nil))
+      (dired-do-rename)))
+
+  (put 'dired-find-alternate-file 'disabled nil))
+
 
 (use-package dired+
   :load-path "~/.emacs.d/plugins/dired+"
+  :after (dired)
   :demand
   ;; :config
   ;; (eval-after-load "dired-aux"
@@ -39,6 +53,7 @@
   )
 
 (use-package dired-rsync
+  :after (dired)
   :config
   (bind-key "C-c C-r" 'dired-rsync dired-mode-map))
 
