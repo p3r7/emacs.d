4ba7193d20cd229cd7ba5e662f0377485b15d4c4
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-08-31 20:42:43 +0200

    Enabling aggressive-indent for all prog-modes (except python)

--- init/components/init-aggressive-indent.el
+++ init/components/init-aggressive-indent.el
@@ -1,5 +1,8 @@
 
 (use-package aggressive-indent
-  :hook emacs-lisp-mode-hook)
+  :hook
+  (prog-mode . aggressive-indent-mode)
+  ;; REVIEW: could be cleaner to: (add-to-list 'aggressive-indent-excluded-modes 'python-mode)
+  (python-mode . (lambda () (aggressive-indent-mode -1))))
 
 (provide 'init-aggressive-indent)
