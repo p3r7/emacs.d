a14e0529d7c462fe912f4379d156298b5b8edb80
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-08-31 21:06:29 +0200

    Don't ask for killing current buffer

--- init/init-buffer-navigation.el
+++ init/init-buffer-navigation.el
@@ -1,11 +1,22 @@
 
 (require 'init-ace-jump)
 
+
 (use-package goto-last-change
   :bind ([(meta p)(u)] . goto-last-change))
 
+
 (use-package goto-line-preview
   :bind ([remap goto-line] . goto-line-preview))
+
+
+(defun prf/kill-this-buffer ()
+  "Kill the current buffer.
+More stable than default `kill-this-buffer'"
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+(global-set-key (kbd "C-x k") #'prf/kill-this-buffer)
 
 
 (provide 'init-buffer-navigation)
