ccd7c4de0225fea32015cb5391ea2c2e938e1a46
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-08-31 20:41:35 +0200

    Reproducting familiar key bindings under CIDER

--- init/lang/init-clojure.el
+++ init/lang/init-clojure.el
@@ -2,14 +2,30 @@
 (use-package clojure-mode)
 (use-package cider
   :demand
-  :bind (:map cider-repl-mode-map
+  :after (clojure-mode)
+  :bind (:map cider-mode-map
+	      ("C-h f" . cider-doc)
+	      ("C-h C-f" . cider-find-var)
+	      :map cider-repl-mode-map
+	      ("C-h f" . cider-doc)
+	      ("C-h C-f" . cider-find-var)
+	      ("C-c E" . cider-repl-clear-buffer)
 	      ("C-x k" . cider-quit)
 	      ("C-x C-k" . cider-quit))
-  :after (clojure-mode)
   :init
   (setq org-babel-clojure-backend 'cider)
   :config
-  (require 'ob-clojure))
+  (require 'ob-clojure)
+
+  ;; stolen from https://github.com/codahale/emacs.d/blob/master/init.el
+  (defun prf/cider-repl-reset ()
+    (interactive)
+    (save-some-buffers)
+    (cider-interactive-eval
+     (concat "((or (resolve 'user/reset)"
+             "     (resolve 'clojure.tools.namespace.repl/refresh)))")))
+  (define-key cider-mode-map (kbd "C-c C-x") 'prf/cider-repl-reset)
+  (define-key clojure-mode-map (kbd "C-c C-x") 'prf/cider-repl-reset))
 
 (use-package cider-eval-sexp-fu
   :after (cider))
