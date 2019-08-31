e2749b7ee9c96b1b46c8ecd6fb74b723d0e54341
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-08-31 22:18:44 +0200

    Added undo-tree

--- init/init-buffer-navigation.el
+++ init/init-buffer-navigation.el
@@ -1,14 +1,33 @@
 
-(require 'init-ace-jump)
-
-
-(use-package goto-last-change
-  :bind ([(meta p)(u)] . goto-last-change))
-
+;; ------------------------------------------------------------------------
+;; moving around
 
 (use-package goto-line-preview
   :bind ([remap goto-line] . goto-line-preview))
 
+(require 'init-ace-jump)
+
+
+;; ------------------------------------------------------------------------
+;; undo history
+
+(use-package goto-last-change
+  :bind ([(meta p)(u)] . goto-last-change))
+
+(use-package undo-tree
+  ;; The visual tree feature is great, but I have trouble adjusting to
+  ;; the change of behaviour (no C-g to reverse direction)
+  :disabled
+  :demand
+  :config
+  (global-undo-tree-mode 1)
+  (defalias 'redo 'undo-tree-redo))
+
+;; REVIEW: timer trick https://emacs.stackexchange.com/a/47349
+
+
+;; ------------------------------------------------------------------------
+;; killing
 
 (defun prf/kill-this-buffer ()
   "Kill the current buffer.
