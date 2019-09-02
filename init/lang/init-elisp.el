86ffd28c11d89941af7863538864d1f4d2bccd53
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-09-02 13:09:19 +0200

    Cleaner elisp config

--- init/lang/init-elisp.el
+++ init/lang/init-elisp.el
@@ -1,7 +1,7 @@
 
-(eval-after-load "dash" '(dash-enable-font-lock))
 
-;; stolen from John Wiegley
+;; stolen from John Wiegley (https://github.com/jwiegley/dot-emacs/blob/master/init.el)
+;; REVIEW: most of those have a global scope and not an elisp scope, should move them somewhere else
 (bind-key "C-c e b" #'eval-buffer)
 (bind-key "C-c e e" #'toggle-debug-on-error)
 (bind-key "C-c e f" #'emacs-lisp-byte-compile)
@@ -17,13 +17,22 @@
 (bind-key "C-h e v" #'find-variable)
 (bind-key "C-h e V" #'apropos-value)
 
+;; REVIEW: I'vs seen this in others' config
+;; (use-package lisp-mode
+;;   :commands emacs-lisp-mode)
+
+(use-package emacs-lisp-mode
+  :ensure nil
+  :interpreter ("emacs" . emacs-lisp-mode))
+
 (use-package macrostep
+  :after (emacs-lisp-mode)
   :bind (:map emacs-lisp-mode-map
 	      ("C-c e m" . macrostep-expand)))
 
 (use-package eval-expr
-  :bind ("M-:" . eval-expr)
   :after (paredit)
+  :bind ("M-:" . eval-expr)
   :config
   (defun eval-expr-minibuffer-setup ()
     (local-set-key (kbd "<tab>") #'lisp-complete-symbol)
@@ -31,6 +40,7 @@
     (paredit-mode)))
 
 (use-package elisp-slime-nav
+  :after (paredit)
   :diminish
   :commands (elisp-slime-nav-mode
              elisp-slime-nav-find-elisp-thing-at-point))
