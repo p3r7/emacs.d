1e8a1bd06d707e7ec55b30af9613234a4bad5943
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-09-10 14:41:26 +0200

    Replicate ace-jump-mode visual experience

--- init/components/init-avy.el
+++ init/components/init-avy.el
@@ -8,9 +8,14 @@
 	 ("C-c C-SPC" . avy-goto-word-1))
   :init
   (setq avy-background t)
-  ;; TODO: I got used to ace-jump-mode faces:
-  ;; ace-jump-face-background and ace-jump-face-foreground
-  )
+  :config
+  ;; set defautl face to value of ace-jump-face-foreground
+  (custom-set-faces
+   '(avy-lead-face
+     ((((class color)) (:foreground "red" :background nil :underline nil))
+      (((background dark)) (:foreground "gray100" :background nil :underline nil))
+      (((background light)) (:foreground "gray0" :background nil :underline nil))
+      (t (:foreground "gray100" :background nil :underline nil))))))
 
 
 (provide 'init-avy)
