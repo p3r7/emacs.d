f55220f92c1e0ccac8c4c98b013b478aa22d4889
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-09-24 14:30:48 +0200

    Uniformize key bindings for killing buffer

--- init/components/init-magit.el
+++ init/components/init-magit.el
@@ -7,8 +7,12 @@
   ;; (magit-git-executable "C:/cygwin64/usr/libexec/git-core/git.exe")
   ;; (magit-git-executable "c:/cygwin64/bin/git.exe")
   ;; (magit-git-executable "C:/Program Files/Git/cmd/git.EXE")
-  :bind (:map dired-mode-map
-	      ("C-x g" . magit-status))
+  :bind (
+         :map dired-mode-map
+         ("C-x g" . magit-status)
+         :map with-editor-mode-map
+         ("C-x k" . with-editor-cancel)
+         ("C-x C-k" . with-editor-cancel))
   :init
   (setq magit-auto-revert-mode 1
 	magit-last-seen-setup-instructions "1.4.0")
