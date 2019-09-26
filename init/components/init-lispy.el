dc723702970d934c4965ca9b8653a9944088ea08
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-09-26 20:15:56 +0200

    Merging changes

--- init/components/init-lispy.el
+++ init/components/init-lispy.el
@@ -23,22 +23,32 @@
         (unbind-key "\"" lispy-mode-map-lispy)
         (unbind-key "\"" lispy-mode-map-lispy)
 
+        (unbind-key "M-m" lispy-mode-map-lispy) ; lispy-mark-symbol
         (unbind-key "C-d" lispy-mode-map-lispy)
         (unbind-key "DEL" lispy-mode-map-lispy))))
+
   :bind (
-         ;; :map lispy-mode-map
-         ;; ("C-c DEL" . lispy-delete-backward)
          :map lispy-mode-map-lispy
-         ("C-c DEL" . lispy-delete-backward))
+         ("C-c DEL" . lispy-delete-backward)
+         ;; disable when region active
+         ("SPC" . prf/lispy-space-no-selection))
+
   :init
   (setq lispy-compat '(edebug macrostep cider))
+
   :config
+  (defun prf/lispy-space-no-selection ()
+    (interactive)
+    (if (region-active-p)
+        (insert "")
+      (lispy-space)))
+
   ;; enable in minibuffer
   (defun lispy-minibuffer-eval-exp-hook ()
     (when (eq this-command 'eval-expression)
       (lispy-mode 1)))
   (add-hook 'minibuffer-setup-hook #'lispy-minibuffer-eval-exp-hook)
-  ;; disable enabling when region active
+  ;; disable when region active
   (advice-add 'delete-selection-pre-hook :around 'lispy--delsel-advice))
 
 
