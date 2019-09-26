62ba281a00305f7fdc7ca1db6b7024764fbace67
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-09-26 15:27:02 +0200

    Remove distracting hl of current parens block

--- init/components/init-smartparens.el
+++ init/components/init-smartparens.el
@@ -2,6 +2,8 @@
 (use-package smartparens
   :config
   (require 'smartparens-config)
+  (set-face-attribute 'sp-pair-overlay-face nil
+                      :inherit nil)
   (smartparens-global-mode 1))
 
 (provide 'init-smartparens)
