a7ea2b074781a4530bc70738a4f264f53c2b80e8
perf <11557146+p3r7@users.noreply.github.com>
2019-10-02 13:46:44 +0200

    Dynamic switch from ac to company + conditional enable py

--- init/components/init-eglot.el
+++ init/components/init-eglot.el
@@ -1,16 +1,23 @@
 
+(defun prf/hook/eglot-python-maybe ()
+  (when (executable-find "pyls")
+    (eglot-ensure)))
+
 (use-package eglot
   :after (company)
   :commands (eglot eglot-ensure)
-  ;; :hook ((c-mode . eglot-ensure))
+  :hook ((python-mode . prf/hook/eglot-python-maybe))
   :config
   (defun prf/hook/prog-mode/company-when-eglot ()
-    (when (and (fboundp 'company-mode)
-               eglot--managed-mode)
+    (cond
+     (eglot--managed-mode
       (company-mode 1)
-      (auto-complete-mode -1)))
+      (auto-complete-mode -1))
+     (t
+      (company-mode -1)
+      (auto-complete-mode 1))))
 
-  (add-hook 'prog-mode-hook #'prf/hook/prog-mode/company-when-eglot))
+  (add-hook 'eglot--managed-mode-hook #'prf/hook/prog-mode/company-when-eglot))
 
 
 (provide 'init-eglot)
