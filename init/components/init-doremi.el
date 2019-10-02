130454cec40376e8343686e00c3dc291a2b93d7a
perf <11557146+p3r7@users.noreply.github.com>
2019-10-02 17:30:16 +0200

    Usaing latest version of hexrgb (stable is buggy)

--- init/components/init-doremi.el
+++ init/components/init-doremi.el
@@ -4,29 +4,41 @@
 ;; LIB DEPENDENCIES
 
 (use-package faces+
+  :defer t
   :quelpa (faces+ :fetcher github :repo "emacsmirror/faces-plus"))
 
 (use-package frame-fns
+  :defer t
   :quelpa (frame-fns :fetcher github :repo "emacsmirror/frame-fns"))
+
+(use-package hexrgb
+  :defer t
+  :quelpa (hexrgb :fetcher github :repo "emacsmirror/hexrgb"))
 
 
 
 ;; MAIN
 
 (use-package doremi
+  :defer t
   :quelpa (doremi :fetcher github :repo "emacsmirror/doremi"))
 
 (use-package doremi-mac
+  :defer t
   :quelpa (doremi-mac :fetcher github :repo "emacsmirror/doremi-mac")
   :after (doremi))
 
 (use-package doremi-frm
+  :defer t
   :quelpa (doremi-frm :fetcher github :repo "emacsmirror/doremi-frm")
-  :after (doremi faces+ frame-fns))
+  :after (doremi faces+ frame-fns hexrgb))
 
 (use-package doremi-cmd
+  :defer t
   :quelpa (doremi-cmd :fetcher github :repo "emacsmirror/doremi-cmd")
   :after (doremi))
 
+
+
 
 (provide 'init-doremi)
