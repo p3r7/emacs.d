a5a2d4901a43a560a8d7a59ba4ab3d98a612d17b
perf <11557146+p3r7@users.noreply.github.com>
2019-10-03 10:53:40 +0200

    Fix handling helm not loaded

--- plugins/ansible-tramp/ansible-tramp.el
+++ plugins/ansible-tramp/ansible-tramp.el
@@ -101,33 +101,34 @@
 
 ;; http://kitchingroup.cheme.cmu.edu/blog/2015/01/30/More-adventures-in-helm-more-than-one-action/
 
-(setq ansible-tramp-helm-source
-      (helm-build-sync-source "Connect to Ansible host"
-	:candidates #'ansible-tramp-get-inventory-hostnames
-	:fuzzy-match t
-	:action '(("shell" .
-		  (lambda (candidate)
-		    (let ((host-address (ansible-tramp-get-inventory-address-for-host candidate)))
-		      (prf/tramp/remote-shell host-address))))
-		 ("dired" .
-		  (lambda (candidate)
-		    (let ((host-address (ansible-tramp-get-inventory-address-for-host candidate)))
-		      (prf/tramp/remote-shell host-address ))))
-		 ("debug" .
-		  (lambda (candidate)
-		    (let ((hostname candidate)
-			  (host-address (ansible-tramp-get-inventory-address-for-host candidate)))
-		      (message-box "selected: %s (%s)" hostname host-address)))))))
-
-
 (eval-after-load "helm"
-  '(defun helm-ansible-inventory-host-connect ()
-     (interactive)
-     (let ((helm-candidate-number-limit 10000))
-       (helm :sources '(ansible-tramp-helm-source)
-	     :input (let ((tap (thing-at-point 'symbol)))
-		      (when (member tap (ansible-tramp-get-inventory-hostnames)) tap))
-	     :buffer "*helm Tramp Shell Ansible*"))))
+  '(progn
+
+     (setq ansible-tramp-helm-source
+           (helm-build-sync-source "Connect to Ansible host"
+             :candidates #'ansible-tramp-get-inventory-hostnames
+             :fuzzy-match t
+             :action '(("shell" .
+                        (lambda (candidate)
+                          (let ((host-address (ansible-tramp-get-inventory-address-for-host candidate)))
+                            (prf/tramp/remote-shell host-address))))
+                       ("dired" .
+                        (lambda (candidate)
+                          (let ((host-address (ansible-tramp-get-inventory-address-for-host candidate)))
+                            (prf/tramp/remote-shell host-address ))))
+                       ("debug" .
+                        (lambda (candidate)
+                          (let ((hostname candidate)
+                                (host-address (ansible-tramp-get-inventory-address-for-host candidate)))
+                            (message-box "selected: %s (%s)" hostname host-address)))))))
+
+     (defun helm-ansible-inventory-host-connect ()
+       (interactive)
+       (let ((helm-candidate-number-limit 10000))
+         (helm :sources '(ansible-tramp-helm-source)
+               :input (let ((tap (thing-at-point 'symbol)))
+                        (when (member tap (ansible-tramp-get-inventory-hostnames)) tap))
+               :buffer "*helm Tramp Shell Ansible*")))))
 
 
 ;; ------------------------------------------------------------------------
