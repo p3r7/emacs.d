dde2450443b4767dfd2671b30b964e9ae7c99651
perf <11557146+p3r7@users.noreply.github.com>
2019-10-04 10:59:31 +0200

    Fic bad condition

--- plugins/tldr-ext/tldr-ext.el
+++ plugins/tldr-ext/tldr-ext.el
@@ -1,13 +1,19 @@
+;;; tldr-ext.el --- extension to tldr.el             -*- lexical-binding: t; -*-
+
+;; Author: Jordan Besly
+;; Keywords: tools, docs
+;; Package-Requires: ((emacs "24.3") (request "0.3.0"))
+;; Version: {{VERSION}}
+
+(require 'tldr)
 
 
-;; ------------------------------------------------------------------------
-;; VARS
+ ;; VARS
 
 (defvar tldr-ext-directory-path nil)
 
 
-;; ------------------------------------------------------------------------
-;; FUNCTIONS
+;; FUNCTIONS
 
 (defun prf/tldr-ext-render-markdown (command)
   (let ((tldr-directory-path tldr-ext-directory-path))
@@ -27,29 +33,27 @@
     (tldr-get-commands-list)))
 
 
-;; ------------------------------------------------------------------------
-;; INIT
+;; INIT
 
 (defun tldr-ext-activate ()
   (interactive)
-  (if (and tldr-ext-directory-path
-	   (file-directory-p tldr-ext-directory-path))
-      (defadvice tldr-get-commands-list (around tldr-get-commands-list-with-ext activate)
-	(let ((command-list ad-do-it))
-	  (let* ((tldr-directory-path tldr-ext-directory-path)
-		 (ext-command-list (funcall (ad-get-orig-definition 'tldr-get-commands-list))))
-	    (setq ad-return-value (delq nil (delete-dups (append command-list ext-command-list)))))))
+  (if (not (and tldr-ext-directory-path
+                (file-directory-p tldr-ext-directory-path)))
+      (message "Path of var `tldr-ext-directory-path' %s not found on disk" tldr-ext-directory-path)
+    (defadvice tldr-get-commands-list (around tldr-get-commands-list-with-ext activate)
+      (let ((command-list ad-do-it))
+        (let* ((tldr-directory-path tldr-ext-directory-path)
+               (ext-command-list (funcall (ad-get-orig-definition 'tldr-get-commands-list))))
+          (setq ad-return-value (delq nil (delete-dups (append command-list ext-command-list)))))))
 
-      (defadvice tldr-render-markdown (around tldr-render-markdown-with-ext activate)
-	(let ((md ad-do-it))
-	  (let* ((tldr-directory-path tldr-ext-directory-path)
-		 (tldr-directory-file-path (tldr-get-file-path-from-command-name (ad-get-arg 0)))
-		 md-ext)
-	    (when tldr-directory-file-path
-	      (setq md-ext (funcall (ad-get-orig-definition 'tldr-render-markdown) (ad-get-arg 0))))
-	    (setq ad-return-value (concat md md-ext)))))
-
-    (message "Path of var `tldr-ext-directory-path' %s not found on disk" tldr-ext-directory-path)))
+    (defadvice tldr-render-markdown (around tldr-render-markdown-with-ext activate)
+      (let ((md ad-do-it))
+        (let* ((tldr-directory-path tldr-ext-directory-path)
+               (tldr-directory-file-path (tldr-get-file-path-from-command-name (ad-get-arg 0)))
+               md-ext)
+          (when tldr-directory-file-path
+            (setq md-ext (funcall (ad-get-orig-definition 'tldr-render-markdown) (ad-get-arg 0))))
+          (setq ad-return-value (concat md md-ext)))))))
 
 
 (provide 'tldr-ext)
