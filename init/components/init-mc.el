230c6b3bb1fd3af3fbd95a1f70e2a372e1279a3f
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-10-21 18:32:08 +0200

    Ensure compatibility w/ cua-mode

--- init/components/init-mc.el
+++ init/components/init-mc.el
@@ -1,13 +1,16 @@
 
 
 (use-package multiple-cursors
+  :demand
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
          ("C-c C-<" . mc/mark-all-like-this))
   :config
-  (with-eval-after-load 'cua-mode
-    (unbind-key "C-S-c" cua--region-keymap)))
+  (if (boundp 'cua-mode)
+      (unbind-key "C-S-c" cua--region-keymap)
+    (with-eval-after-load 'cua-mode
+      (unbind-key "C-S-c" cua--region-keymap))))
 
 
 
