291d69bdee2daa953ee4009d0d721fdad6668890
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-10-21 08:20:16 +0200

    FIx issue w/ cua-mode

--- init/components/init-mc.el
+++ init/components/init-mc.el
@@ -4,9 +4,12 @@
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)))
+         ("C-c C-<" . mc/mark-all-like-this))
+  :config
+  (with-eval-after-load 'cua-mode
+    (unbind-key "C-S-c" cua--region-keymap)))
 
 
 
 
-(provide init-mc)
+(provide 'init-mc)
