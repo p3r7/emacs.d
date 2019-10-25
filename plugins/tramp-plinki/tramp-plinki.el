9c10dedcc26ff2a9cbd86781ac95fb03232742cb
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-10-25 12:10:42 +0200

    Cleaner code, fix to use cert during copy

--- plugins/tramp-plinki/tramp-plinki.el
+++ plugins/tramp-plinki/tramp-plinki.el
@@ -1,11 +1,15 @@
 
+(require 'tramp)
 (require 'dash)
+(require 'subr-x)
 
 (defvar tramp-plinki-ppk nil)
 
+(defvar tramp-plinki-methods '("pscp" "plink" "plinkx" "psftp"))
+
 
 ;; ------------------------------------------------------------------------
-;; MODIFY EXISTING TRAMP METHODS
+;; MODIFY EXISTING TRAMP METHODS: ARGS
 
 (defun tramp-plinki--add-certificate-login-arg (tramp-login-args)
   (let ((login-args (car (cdr tramp-login-args))))
@@ -13,6 +17,17 @@
         tramp-login-args
       (add-to-list 'login-args `("-i" ,(concat "\"" tramp-plinki-ppk "\"")))
       `(tramp-login-args ,login-args))))
+
+(defun tramp-plinki--add-certificate-copy-arg (tramp-copy-args)
+  (let ((copy-args (car (cdr tramp-copy-args))))
+    (if (string= "" tramp-plinki-ppk)
+        tramp-copy-args
+      (add-to-list 'copy-args `("-i" ,(concat "\"" tramp-plinki-ppk "\"")))
+      `(tramp-copy-args ,copy-args))))
+
+
+;; ------------------------------------------------------------------------
+;; MODIFY EXISTING TRAMP METHODS: METHOD
 
 ;; REVIEW: seems to eval whole method-def-args, which is unwanted
 (defun tramp-plinki--add-certificate-login-arg-to-method (tramp-method-def)
@@ -24,14 +39,50 @@
            #'tramp-plinki--add-certificate-login-arg
            method-def-args))))
 
+(defun tramp-plinki--add-certificate-copy-arg-to-method (tramp-method-def)
+  (let ((method-name (car tramp-method-def))
+        (method-def-args (cdr tramp-method-def)))
+    (cons method-name
+          (-map-when
+           (lambda (e) (equal (car e) 'tramp-copy-args))
+           #'tramp-plinki--add-certificate-copy-arg
+           method-def-args))))
+
+(defun tramp-plinki--suffix-method-name (tramp-method-def)
+  (let ((method-name (car tramp-method-def))
+        (method-def-args (cdr tramp-method-def)))
+    (cons (concat method-name "i")
+          method-def-args)))
+
+(defun tramp-plinki--register-tramp-completion-fun (tramp-method-def)
+  (let ((method-name (car tramp-method-def)))
+    (tramp-set-completion-function (concat method-name "i") tramp-completion-function-alist-ssh)
+    tramp-method-def))
+
+
+;; ------------------------------------------------------------------------
+;; MODIFY EXISTING TRAMP METHODS: METHOD LIST
+
 (defun tramp-plinki--get-enriched-tramp-methods ()
   (-map-when
-   (lambda (e) (member (car e) '("pscp" "plink" "plinkx")))
-   #'tramp-plinki--add-certificate-login-arg-to-method
+   (lambda (e) (member (car e) tramp-plinki-methods))
+   (lambda (e) (thread-first e
+            (tramp-plinki--add-certificate-login-arg-to-method)
+            (tramp-plinki--add-certificate-copy-arg-to-method)))
    tramp-methods))
 
 (defun tramp-plinki-enrich-existing ()
   (setq tramp-methods (tramp-plinki--get-enriched-tramp-methods)))
+
+(defun tramp-plinki--get-enriched-with-new-tramp-methods ()
+  (-map-when
+   (lambda (e) (member (car e) tramp-plinki-methods))
+   (lambda (e) (thread-first e
+            ;; (tramp-plinki--register-tramp-completion-fun)
+            (tramp-plinki--suffix-method-name)
+            (tramp-plinki--add-certificate-login-arg-to-method)
+            (tramp-plinki--add-certificate-copy-arg-to-method)))
+   tramp-methods))
 
 
 ;; ------------------------------------------------------------------------
@@ -41,45 +92,15 @@
   (when (not tramp-plinki-ppk)
     (error "empty value for tramp-plinki-ppk"))
 
-  (add-to-list 'tramp-methods
-	       `("plinki"
-		 (tramp-login-program        "plink")
-		 ;; ("%h") must be a single element, see `tramp-compute-multi-hops'.
-		 (tramp-login-args           (("-l" "%u") ("-P" "%p") ("-ssh") ("-t")
-					      ("-i" ,(concat "\"" tramp-plinki-ppk "\""))
-					      ("%h") ("\"")
-					      (,(format
-						 "env 'TERM=%s' 'PROMPT_COMMAND=' 'PS1=%s'"
-						 tramp-terminal-type
-						 tramp-initial-end-of-output))
-					      ("/bin/sh") ("\"")))
-		 (tramp-remote-shell         "/bin/sh")
-		 (tramp-remote-shell-login   ("-l"))
-		 (tramp-remote-shell-args    ("-c"))))
-  (add-to-list 'tramp-methods
-	       `("pscpi"
-		 (tramp-login-program        "plink")
-		 (tramp-login-args           (("-l" "%u") ("-P" "%p") ("-ssh") ("-t")
-					      ("-i" ,(concat "\"" tramp-plinki-ppk "\""))
-					      ("%h") ("\"")
-					      (,(format
-						 "env 'TERM=%s' 'PROMPT_COMMAND=' 'PS1=%s'"
-						 tramp-terminal-type
-						 tramp-initial-end-of-output))
-					      ("/bin/sh") ("\"")))
-		 (tramp-remote-shell         "/bin/sh")
-		 (tramp-remote-shell-login   ("-l"))
-		 (tramp-remote-shell-args    ("-c"))
-		 (tramp-copy-program         "pscp")
-		 (tramp-copy-args            (("-l" "%u") ("-P" "%p") ("-scp") ("-p" "%k")
-					      ("-q") ("-r")))
-		 (tramp-copy-keep-date       t)
-		 (tramp-copy-recursive       t)))
+  (setq tramp-methods (tramp-plinki--get-enriched-with-new-tramp-methods))
 
-  (eval-after-load 'tramp
-    '(progn
-       (tramp-set-completion-function "plinki" tramp-completion-function-alist-ssh)
-       (tramp-set-completion-function "pscpi" tramp-completion-function-alist-ssh))))
+  (-map-when
+   (lambda (e) (member (car e) tramp-plinki-methods))
+   (lambda (e)
+     (let ((method-name (car tramp-method-def))
+           (tramp-set-completion-function (concat method-name "i") tramp-completion-function-alist-ssh)))
+     nil)
+   tramp-methods))
 
 
 (provide 'tramp-plinki)
