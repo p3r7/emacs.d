774f1d37e993afac968a86c910df1187499684c6
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-10-29 14:46:02 +0100

    Better interactive json formating

--- init/format/init-json.el
+++ init/format/init-json.el
@@ -1,11 +1,28 @@
 
-(if (fboundp 'json-pretty-print)
-    (defalias 'json/format 'json-pretty-print)
-  (if (executable-find "python")
-      (defun json/format ()
-	(interactive)
-	(save-excursion
-	  (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)))))
 
+(if (fboundp #'json-pretty-print)
+    (defalias 'json/format-region #'json-pretty-print)
+  (when (executable-find "python")
+    (defun json/format-region (begin end)
+      "Pretty-print selected region."
+      (interactive "r")
+      (save-excursion
+        (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)))))
+
+
+(when (fboundp #'json/format-region)
+  (defun json/format ()
+    (interactive)
+    (save-excursion
+      (when (not (region-active-p))
+        (let* ((matching-prev (scan-lists (point) -1 0))
+               (matching-next (scan-lists (point) 1 0))
+               (matching (or matching-prev matching-next)))
+          (goto-char matching)
+          (activate-mark)))
+      (call-interactively #'json/format-region))))
+
+
+
 
 (provide 'init-json)
