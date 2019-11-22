9c481e4da0c9764b244c5f69e28f2c1c7c502056
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-11-22 16:10:20 +0100

    Added mouse shortcut to add mc cursor
    Gotten from http://pragmaticemacs.com/emacs/add-multiple-cursors-with-mouse-clicks/

--- init/components/init-mc.el
+++ init/components/init-mc.el
@@ -5,7 +5,8 @@
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this))
+         ("C-c C-<" . mc/mark-all-like-this)
+         ("C-S-<mouse-1>" . mc/add-cursor-on-click))
   :config
   (if (boundp 'cua-mode)
       (unbind-key "C-S-c" cua--region-keymap)
