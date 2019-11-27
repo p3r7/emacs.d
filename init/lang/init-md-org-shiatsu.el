4c13a21e8e9791e1024921130012640e265c7656
jordan.besly <11557146+p3r7@users.noreply.github.com>
2019-11-27 12:19:14 +0100

    Prevent double exec lookup + sharp quoting fun

--- init/lang/init-md-org-shiatsu.el
+++ init/lang/init-md-org-shiatsu.el
@@ -5,11 +5,11 @@
   :config
   (defun prf/md-hook/enable-md-shiatsu-minor-mode ()
     (when (string-match-p (regexp-quote "shiatsu") default-directory)
-      (when (executable-find "multimarkdown")
-	(setq markdown-command (executable-find "multimarkdown")))
+      (let ((mmd (executable-find "multimarkdown")))
+        (when mmd (setq markdown-command mmd)))
       (prf-md-shiatsu 1)))
 
-  (add-hook 'markdown-mode-hook 'prf/md-hook/enable-md-shiatsu-minor-mode))
+  (add-hook 'markdown-mode-hook #'prf/md-hook/enable-md-shiatsu-minor-mode))
 
 
 (provide 'init-md-org-shiatsu)
