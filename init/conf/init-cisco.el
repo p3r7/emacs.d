9319bc5f6e78d64682193dc0229f61016f035248
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-11-28 21:29:52 +0100

    New support for conf cisco & fluxbox, removed old src

---
+++ init/conf/init-cisco.el
@@ -0,0 +1,9 @@
+
+
+(use-package cisco-router-mode
+  :quelpa (cisco-router-mode :fetcher github :repo "emacsmirror/cisco-router-mode"))
+
+
+
+
+(provide 'init-cisco)
---
+++ init/conf/init-fluxbox-conf.el
@@ -0,0 +1,8 @@
+
+(use-package fluxbox-mode
+  :load-path "~/.emacs.d/plugins/fluxbox-mode")
+
+
+
+
+(provide 'init-fluxbox-conf)
