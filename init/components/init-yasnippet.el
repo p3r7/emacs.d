2a0b32cde8c75978de1c3ba0c6204a39ff9e2c9d
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-11-30 12:35:23 +0100

    Working cancel snippet on key repeat

--- init/components/init-yasnippet.el
+++ init/components/init-yasnippet.el
@@ -17,9 +17,19 @@
 
   ;; NB: this prevents nested expansions
 
+  (defvar prf/yas-last-expand-success-timestamp nil)
+  (defvar prf/yas-cancel-on-repeat-delay 1 "Max time to wait for allowing to cancel snippet expansion")
+
+  (defun prf/yas-update-last-expand-success-timestamp ()
+    (setq prf/yas-last-expand-success-timestamp (float-time)))
+
+  (add-hook 'yas-before-expand-snippet-hook #'prf/yas-update-last-expand-success-timestamp)
+
   (defun prf/yas/just-expanded-p ()
     ;; TODO: test also finished expansion that just ended
-    (yas-active-snippets))
+    (or (yas-active-snippets)
+        (>= prf/yas-cancel-on-repeat-delay
+            (- (float-time) prf/yas-last-expand-success-timestamp))))
 
   (defun prf/yas-maybe-expand-or-cancel-key-filter (cmd)
     (if (prf/yas/just-expanded-p)
@@ -28,12 +38,17 @@
 
   (defun prf/yas-expand-or-cancel ()
     (interactive)
-    (if (yas-active-snippets)
-        (progn
-          (yas-abort-snippet (car (yas-active-snippets)))
-          (undo)
-          (just-one-space)))
-    (yas-expand))
+    (cond
+     ((yas-active-snippets)
+      (yas-abort-snippet (car (yas-active-snippets)))
+      (undo 1)
+      (just-one-space))
+     ((>= prf/yas-cancel-on-repeat-delay
+          (- (float-time) prf/yas-last-expand-success-timestamp))
+      (undo 1)
+      (just-one-space))
+     (t
+      (yas-expand))))
 
   (defconst prf/yas-maybe-expand-or-cancel
     '(menu-item "" prf/yas-expand-or-cancel :filter prf/yas-maybe-expand-or-cancel-key-filter)
@@ -44,6 +59,7 @@ and `yas-abort-snippet' (already during expansion).
 Inspired by `yas-maybe-expand'")
 
   ;; (define-key yas-minor-mode-map (kbd "SPC") prf/yas-maybe-expand-or-cancel)
+  ;; (define-key yas-minor-mode-map (kbd "SPC") nil)
 
   (yas-global-mode 1))
 
