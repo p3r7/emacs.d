0f1ac033164b962c7fa186aecf580569a7e3a7df
p3r7 <11557146+p3r7@users.noreply.github.com>
2019-12-14 15:48:17 +0100

    (cont.)

--- init/lang/init-rest.el
+++ init/lang/init-rest.el
@@ -1,6 +1,10 @@
 
 
-(use-package restclient)
+(use-package restclient
+  :init
+  (unless restclient-use-org
+    (add-to-list 'auto-mode-alist '("\\.rest\\'" . restclient-mode))
+    (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))))
 
 
 
