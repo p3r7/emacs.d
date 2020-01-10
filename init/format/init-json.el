59d132a46a06858476c4368d196a5f665427b22a
jordan.besly <11557146+p3r7@users.noreply.github.com>
2020-01-10 17:14:40 +0100

    More robust case region active

--- init/format/init-json.el
+++ init/format/init-json.el
@@ -1,6 +1,7 @@
 
 (require 'paren)
 
+;; REVIEW: maybe use lower-level `syntax-ppss' ?
 (defun prf/parens/get-siblings ()
   "Returns a 3 element list: (DIRECTION FIRST-POS LAST-POS), where direction is relative to current pos (i.e. (point))"
   (let ((parens-couple-pos (show-paren--default)))
@@ -17,30 +18,35 @@
     (defalias 'json/format-region #'json-pretty-print)
   (when (executable-find "python")
     (defun json/format-region (begin end)
-      "Pretty-print selected region."
+      "Pretty-print selected json region."
       (interactive "r")
       (save-excursion
         (shell-command-on-region (mark) (point) "python -m json.tool" (buffer-name) t)))))
 
-
 (when (fboundp #'json/format-region)
   (defun json/format ()
+    "Pretty-print json near point or region"
     (interactive)
-    (if (not (region-active-p))
-        (let* ((siblings (prf/parens/get-siblings))
-               current matching)
-          (when siblings
-            (if (eq 1 (car siblings))
-                (progn
-                  (setq current (nth 1 siblings))
-                  (setq matching (nth 2 siblings)))
-              (setq current (nth 2 siblings))
-              (setq matching (nth 1 siblings)))
-            (set-mark current)
-            (goto-char matching)
-            (activate-mark)
-            (call-interactively #'json/format-region)))
-      (call-interactively #'json/format-region))))
+    (let* ((siblings (prf/parens/get-siblings))
+           (dir (car siblings))
+           (first-pos (nth 1 siblings))
+           (last-pos (nth 2 siblings))
+           current matching)
+      (when siblings
+        (if (eq 1 dir)
+            (progn
+              (setq current first-pos)
+              (setq matching last-pos))
+          (setq current last-pos)
+          (setq matching first-pos))
+        (if (region-active-p)
+            (unless (and (eq first-pos (region-beginning))
+                         (eq last-pos (region-end)))
+              (error "Region not json"))
+          (set-mark matching)
+          (goto-char current)
+          (activate-mark))
+        (call-interactively #'json/format-region)))))
 
 
 
