59e7e78d9a009c5af3e505cf908bc91faf57d7bf
jordan.besly <11557146+p3r7@users.noreply.github.com>
2020-01-10 14:59:41 +0100

    Even smarter interractive json decode

--- init/format/init-json.el
+++ init/format/init-json.el
@@ -1,4 +1,17 @@
 
+(require 'paren)
+
+(defun prf/parens/get-siblings ()
+  "Returns a 3 element list: (DIRECTION FIRST-POS LAST-POS), where direction is relative to current pos (i.e. (point))"
+  (let ((parens-couple-pos (show-paren--default)))
+    (when parens-couple-pos
+      (pcase-let ((`(,here-beg ,here-end ,there-beg ,there-end) parens-couple-pos))
+        (if (< there-beg here-beg)
+            (list -1 there-beg here-end)
+          (list 1 here-beg there-end))))))
+
+
+
 
 (if (fboundp #'json-pretty-print)
     (defalias 'json/format-region #'json-pretty-print)
@@ -14,15 +27,19 @@
   (defun json/format ()
     (interactive)
     (if (not (region-active-p))
-        (let* (
-               (matching-prev (scan-lists (point) -1 0))
-               (matching-next (scan-lists (point) 1 0))
-               (matching (or matching-prev matching-next)))
-          (when matching
+        (let* ((siblings (prf/parens/get-siblings))
+               current matching)
+          (when siblings
+            (if (eq 1 (car siblings))
+                (progn
+                  (setq current (nth 1 siblings))
+                  (setq matching (nth 2 siblings)))
+              (setq current (nth 2 siblings))
+              (setq matching (nth 1 siblings)))
+            (set-mark current)
             (goto-char matching)
             (activate-mark)
-            (call-interactively #'json/format-region))
-          )
+            (call-interactively #'json/format-region)))
       (call-interactively #'json/format-region))))
 
 
