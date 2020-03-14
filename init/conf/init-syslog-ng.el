767251460198b701cac906b15d82abf43f4799d8
U-DESKTOP-QBI2N3E\eigen <11557146+p3r7@users.noreply.github.com>
2020-03-14 22:20:02 +0100

    Fix syslog-ng conf mode not in MELPA

--- init/conf/init-syslog-ng.el
+++ init/conf/init-syslog-ng.el
@@ -1,4 +1,5 @@
 (use-package syslogngconf-mode
+  :quelpa (syslogngconf-mode :fetcher github :repo "furiel/syslogngconf-mode")
   :delight "conf[syslog-ng] "
   :magic (("@version: " . syslogngconf-mode))
   :mode (("/scl/.*\\.conf\\'" . syslogngconf-mode)))
