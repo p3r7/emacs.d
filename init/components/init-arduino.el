2eb86ba34833a8c25caec3dad6c1d03c13fd6d6a
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-04-02 13:16:08 +0200

    Dirty fix for c-basic-offset bug

--- init/components/init-arduino.el
+++ init/components/init-arduino.el
@@ -1,8 +1,13 @@
 
 
-(use-package arduino-mode)
+(use-package arduino-mode
+  :config
+  ;; NB: should not be needed, dunno why offest gets reset to 8...
+  (defun prf/arduino-offset-hook ()
+    (setq c-basic-offset 2))
+  (add-hook 'arduino-mode-hook #'prf/arduino-offset-hook))
 
 
 
 
-(provide init-arduino)
+(provide 'init-arduino)
