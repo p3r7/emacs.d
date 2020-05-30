513cfcac04786aa2090b386f9a9afa1ebc0e883e
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-05-30 22:55:40 +0200

    Dep check for eglot

--- init/components/init-eglot.el
+++ init/components/init-eglot.el
@@ -1,24 +1,26 @@
 
-(defun prf/hook/eglot-python-maybe ()
-  (when (executable-find "pyls")
-    (eglot-ensure)))
+(when (version<=  emacs-version "26.3")
 
-(use-package eglot
-  :after (company)
-  :commands (eglot eglot-ensure)
-  :hook ((python-mode . prf/hook/eglot-python-maybe))
-  :config
-  (defun prf/hook/prog-mode/company-when-eglot ()
-    (cond
-     (eglot--managed-mode
-      (company-mode 1)
-      (funcall prf/fav-completion-at-point-mode -1))
-     (t
-      (company-mode -1)
-      (funcall prf/fav-completion-at-point-mode 1))))
+  (use-package eglot
+    :after (company)
+    :commands (eglot eglot-ensure)
+    :hook ((python-mode . prf/hook/eglot-python-maybe))
+    :config
+    (defun prf/hook/eglot-python-maybe ()
+      (when (executable-find "pyls")
+        (eglot-ensure)))
 
-  (when (not (eq prf/fav-completion-at-point 'company))
-    (add-hook 'eglot--managed-mode-hook #'prf/hook/prog-mode/company-when-eglot)))
+    (defun prf/hook/prog-mode/company-when-eglot ()
+      (cond
+       (eglot--managed-mode
+        (company-mode 1)
+        (funcall prf/fav-completion-at-point-mode -1))
+       (t
+        (company-mode -1)
+        (funcall prf/fav-completion-at-point-mode 1))))
+
+    (when (not (eq prf/fav-completion-at-point 'company))
+      (add-hook 'eglot--managed-mode-hook #'prf/hook/prog-mode/company-when-eglot))))
 
 
 
