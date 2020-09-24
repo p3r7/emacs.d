4c5395194665ddf12f50880459d91a83a8989ef3
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-09-24 23:43:52 +0200

    Changed default indent, format

--- init/lang/init-lua.el
+++ init/lang/init-lua.el
@@ -1,8 +1,12 @@
 
 ;; http://stackoverflow.com/questions/4643206/how-to-configure-indentation-in-emacs-lua-mode
 
+
+
 (use-package lua-mode
   :mode "\\.p8$"
+  :init
+  (setq lua-indent-level 2)
   :config
   (defun p8-hook()
     (when (and (buffer-file-name)
@@ -10,8 +14,15 @@
       (setq lua-indent-level 1)))
   (add-hook 'lua-mode-hook 'p8-hook))
 
+
+
+;; PICO-8
+
 (use-package pico8-mode
   :after (lua-mode)
   :quelpa (pico8-mode :fetcher github :repo "Kaali/pico8-mode"))
+
+
+
 
 (provide 'init-lua)
