4e89d324e49973730595ab9c817b04975570abf2
p3r7 <11557146+p3r7@users.noreply.github.com>
2020-11-30 15:39:09 +0100

    Added deadgrep edit keybindings

--- init/components/init-grep.el
+++ init/components/init-grep.el
@@ -9,7 +9,17 @@
 
 (use-package deadgrep
   :if (executable-find "rg")
-  :bind (("<C-find>" . deadgrep)))
+  :bind (("<C-find>" . deadgrep)
+         :map deadgrep-mode-map
+         ("C-c e" . prf/toggle-deadgrep-edit)
+         :map deadgrep-edit-mode-map
+         ("C-c e" . prf/toggle-deadgrep-edit))
+  :config
+  (defun prf/toggle-deadgrep-edit ()
+    (interactive)
+    (if (eq major-mode 'deadgrep-mode)
+        (deadgrep-edit-mode)
+      (deadgrep-mode))))
 
 
 
