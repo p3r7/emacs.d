6f3c886da2d3d5b60caa4e5a3ef4a3240b202c04
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-04-02 17:59:48 +0200

    Disable kw highlight for PHP as causes issues

--- plugins/prf-highlight-keyword/prf-highlight-keyword.el
+++ plugins/prf-highlight-keyword/prf-highlight-keyword.el
@@ -7,7 +7,9 @@
 
 (defvar prd-highlight-keyword-modes
   '(ada-mode c-mode emacs-lisp-mode java-mode haskell-mode web-mode
-             literate-haskell-mode html-mode lisp-mode php-mode python-mode ruby-mode tcl-mode
+             literate-haskell-mode html-mode lisp-mode
+             ;; php-mode
+             python-mode ruby-mode tcl-mode
              scheme-mode sgml-mode sh-mode sml-mode markdown-mode org-mode
              yaml-mode clojure-mode clojurescript-mode clojurec-mode))
 
