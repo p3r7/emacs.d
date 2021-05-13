c7a3c194c363efa0cc6a98dd079204411d45d9dd
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-05-13 12:12:48 +0200

    Add EDN support for jinja2 templates

--- init/lang/init-python.el
+++ init/lang/init-python.el
@@ -24,15 +24,23 @@
   (mmm-add-mode-ext-class 'conf-space-mode "\\.j2\\'" 'jinja2)
   (mmm-add-mode-ext-class 'conf-unix-mode "\\.j2\\'" 'jinja2)
   (mmm-add-mode-ext-class 'conf-windows-mode "\\.j2\\'" 'jinja2)
+
   ;; - toml
   (add-to-list 'auto-mode-alist '("\\.toml\\.j2\\'" . toml-mode))
   (mmm-add-mode-ext-class 'toml-mode "\\.j2\\'" 'jinja2)
+
   ;; - yaml
   (add-to-list 'auto-mode-alist '("\\.\\(e?ya?\\|ra\\)ml\\.j2\\'" . yaml-mode))
   (mmm-add-mode-ext-class 'yaml-mode "\\.j2\\'" 'jinja2)
+
   ;; - .env
   (add-to-list 'auto-mode-alist '("\\.env\\..*\\.j2\\'" . dotenv-mode))
   (mmm-add-mode-ext-class 'dotenv-mode "\\.j2\\'" 'jinja2)
+
+  ;; - .edn
+  (add-to-list 'auto-mode-alist '("\\.edn\\.j2\\'" . clojure-mode))
+  (mmm-add-mode-ext-class 'clojure-mode "\\.j2\\'" 'jinja2)
+
   ;; - shell scripts
   (add-to-list 'auto-mode-alist '("\\.[ckz]?sh\\'\\|\\.shar\\'\\|/\\.z?profile\\.j2\\'" . sh-mode))
   (mmm-add-mode-ext-class 'shell-script-mode "\\.j2\\'" 'jinja2))
