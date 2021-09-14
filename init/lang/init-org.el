b0c71bd84b9bebda00f65a28a76437b43cbe3806
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-09-14 16:57:15 +0200

    leaner lighter

--- init/lang/init-org.el
+++ init/lang/init-org.el
@@ -184,6 +184,7 @@
 (use-package org-roam-ui
   :straight (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
   :after (simple-httpd websocket org-roam)
+  :delight (org-roam-ui-mode "RoamUI")
   :config
   (setq org-roam-ui-sync-theme t
         org-roam-ui-follow t
