85b155565f2c557ab1ca4a8cb71a64c9cee04458
FREELANCE.COM - BESLY <JBESLY@bt0d0000.w2k.bouyguestelecom.fr>
2021-10-05 16:25:09 +0200

    modernise go inteagration (LSP, format on save...)

--- init/lang/init-go.el
+++ init/lang/init-go.el
@@ -1,8 +1,48 @@
 
-(use-package go-mode
-  :defer t)
+(require 'project)
+(require 'eglot)
 
-(use-package go-autocomplete
-  :after (go))
+
+
+
+(use-package go-mode
+  ;; :defer t
+  :after eglot
+  :bind (
+         :map go-mode-map
+         ("C-h C-f" . godef-jump))
+  :hook (
+         (go-mode . (lambda ()
+                      (eglot-ensure)
+                      (setq eglot-workspace-configuration
+                            '((:gopls
+                               . ((staticcheck . t)
+                                  (matcher . "CaseSensitive")))))))
+         (go-mode . (lambda ()
+                      (setq indent-tabs-mode nil
+                            tab-width 2
+                            standard-indent 2)
+                      (add-hook 'before-save-hook #'gofmt-before-save))))
+
+  :config
+  ;; project.el integration
+  (defun project-find-go-module (dir)
+    (when-let ((root (locate-dominating-file dir "go.mod")))
+      (cons 'go-module root)))
+
+  (cl-defmethod project-root ((project (head go-module)))
+    (cdr project))
+
+  (add-hook 'project-find-functions #'project-find-go-module)
+  )
+
+;; NB: gocode is deprecated, using eglot w/ company instead
+;; (use-package go-autocomplete
+;;   :after go)
+
+
+
+
+
 
 (provide 'init-go)
