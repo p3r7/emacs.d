029a1ef4e405956656211675e8c2e3d7bd7a1494
p3r7 <11557146+p3r7@users.noreply.github.com>
2022-02-21 09:24:26 +0100

    norns.el from quelpa

--- init/components/init-norns.el
+++ init/components/init-norns.el
@@ -5,7 +5,8 @@
 
 
 (use-package norns
-  :load-path "~/.emacs.d/plugins/norns"
+  ;; :load-path "~/.emacs.d/plugins/norns"
+  :quelpa (norns :fetcher github :repo "p3r7/norns.el")
   :config
   (add-hook 'lua-mode-hook #'norns-mode-hook)
   (add-hook 'sclang-mode-hook #'norns-mode-hook))
