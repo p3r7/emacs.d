2d1d062e532fbe6db4bf38d872ca4e1f8e922c80
p3r7 <11557146+p3r7@users.noreply.github.com>
2022-02-21 17:54:02 +0100

    use own forked version of kubel that plays better w/ TRAMP

--- init/components/init-kubernetes.el
+++ init/components/init-kubernetes.el
@@ -1,6 +1,8 @@
 
 
-(use-package kubel)
+;; NB: using forked version that does propper lookup on init when accessing a remote (TRAMP) instance
+(use-package kubel
+  :load-path "~/.emacs.d/plugins-spe/kubel-20220104.2320")
 
 
 
