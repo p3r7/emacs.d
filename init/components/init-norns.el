79fae7a2a22ed1c8c8d01d3d58812d71181d9f6a
p3r7 <11557146+p3r7@users.noreply.github.com>
2022-03-16 13:48:55 +0100

    conditional import of norns due to bug in Emacs 28 pretest

--- init/components/init-norns.el
+++ init/components/init-norns.el
@@ -4,12 +4,14 @@
 
 
 
-(use-package norns
-  ;; :load-path "~/.emacs.d/plugins/norns"
-  :quelpa (norns :fetcher github :repo "p3r7/norns.el")
-  :config
-  (add-hook 'lua-mode-hook #'norns-mode-hook)
-  (add-hook 'sclang-mode-hook #'norns-mode-hook))
+;; NB: bug w/ `encode-time' in current pre-release of Emacs 28, breaks `osc'
+(unless (eq emacs-major-version 28)
+  (use-package norns
+    ;; :load-path "~/.emacs.d/plugins/norns"
+    :quelpa (norns :fetcher github :repo "p3r7/norns.el")
+    :config
+    (add-hook 'lua-mode-hook #'norns-mode-hook)
+    (add-hook 'sclang-mode-hook #'norns-mode-hook)))
 
 
 
