518a360d2f1731f1174be9d132747c08e3a4661e
p3r7 <11557146+p3r7@users.noreply.github.com>
2022-03-24 11:09:21 +0100

    now on melpa, better named hook fn

--- init/components/init-norns.el
+++ init/components/init-norns.el
@@ -7,11 +7,9 @@
 ;; NB: bug w/ `encode-time' in current pre-release of Emacs 28, breaks `osc'
 (unless (eq emacs-major-version 28)
   (use-package norns
-    ;; :load-path "~/.emacs.d/plugins/norns"
-    :quelpa (norns :fetcher github :repo "p3r7/norns.el")
     :config
-    (add-hook 'lua-mode-hook #'norns-mode-hook)
-    (add-hook 'sclang-mode-hook #'norns-mode-hook)))
+    (add-hook 'lua-mode-hook #'norns-mode-maybe-activate)
+    (add-hook 'sclang-mode-hook #'norns-mode-maybe-activate)))
 
 
 
