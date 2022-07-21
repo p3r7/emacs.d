86cf15de3db2398457f07a1564cfa8c74cde23a7
p3r7 <11557146+p3r7@users.noreply.github.com>
2022-07-21 10:49:38 +0200

    more aggressive indent blacklist

--- init/components/init-aggressive-indent.el
+++ init/components/init-aggressive-indent.el
@@ -3,6 +3,6 @@
   :hook
   (prog-mode . aggressive-indent-mode)
   ;; REVIEW: could be cleaner to: (add-to-list 'aggressive-indent-excluded-modes 'python-mode)
-  ((python-mode sql-mode sass-mode dotenv-mode dockerfile-mode) . (lambda () (aggressive-indent-mode -1))))
+  ((python-mode sql-mode sass-mode dotenv-mode dockerfile-mode makefile-gmake-mode) . (lambda () (aggressive-indent-mode -1))))
 
 (provide 'init-aggressive-indent)
