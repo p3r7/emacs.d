9604c24e6631bb9e343371627ffe5e873838bf3d
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-04-18 18:06:27 +0200

    quick n dirty fix, seems to be a bc break w/ last aggressive-indent

--- init/components/init-aggressive-indent.el
+++ init/components/init-aggressive-indent.el
@@ -3,8 +3,9 @@
   :hook
   (prog-mode . (lambda ()
                  ;; NB: `aggressive-indent-excluded-modes' only eval'ed when using `global-aggressive-indent-mode'
-                 (unless (cl-member-if #'derived-mode-p aggressive-indent-excluded-modes)
-                   aggressive-indent-mode)))
+                 (when (boundp 'aggressive-indent-excluded-modes)
+                   (unless (cl-member-if #'derived-mode-p aggressive-indent-excluded-modes)
+                     aggressive-indent-mode))))
   :init
   (add-to-list 'aggressive-indent-excluded-modes 'sql-mode)
   (add-to-list 'aggressive-indent-excluded-modes 'sass-mode)
