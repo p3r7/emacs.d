0693bc7edb933fc1ce76d50b1a9a174dde3d9ddd
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-07-30 14:22:58 +0200

    `dumb-jump`, remove deprecated usage, fix TRAMP, fix lua

--- init/components/init-dumb-jump.el
+++ init/components/init-dumb-jump.el
@@ -1,12 +1,78 @@
 
-(use-package dumb-jump
-  :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
-         ("M-g i" . dumb-jump-go-prompt)
-         ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window))
-  :init
-  (setq dumb-jump-selector 'helm))
+;; NB: `dump-jump-go' & variants are deprecated
+;; now have to use `xref-find-definitions' (`M-.') now
 
+;; for rigrprep, install from deb in github repo
+;; Debian/Ubunutu's version isn't compiled w/ the necessary PCRE2 flag
+;; see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=926333
+;;
+;; can bes tested by calling rg w: the --pcre2 opt and getting:
+;; "PCRE2 is not available in this build of ripgrep"
+
+(use-package dumb-jump
+  :init
+  (setq dumb-jump-selector 'helm)
+  (setq dumb-jump-force-searcher 'rg) ; to allow search ouside of project dir
+
+  :config
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+
+  ;; NB: fix for TRAMP
+  ;; patch from https://github.com/jacktasia/dumb-jump/pull/432/files
+  (defun dumb-jump-fetch-results (cur-file proj-root lang _config &optional prompt)
+    "Return a list of results based on current file context and calling grep/ag.
+  CUR-FILE is the path of the current buffer.
+  PROJ-ROOT is that file's root project directory.
+  LANG is a string programming language with CONFIG a property list
+  of project configuration."
+    (let* ((cur-line-num (line-number-at-pos))
+           (proj-config (dumb-jump-get-config proj-root))
+           (config (when (s-ends-with? ".dumbjump" proj-config)
+                     (dumb-jump-read-config proj-root proj-config)))
+           (found-symbol (or prompt (dumb-jump-get-point-symbol)))
+           (look-for (dumb-jump-process-symbol-by-lang lang found-symbol))
+           (pt-ctx (if prompt
+                       (get-text-property 0 :dumb-jump-ctx prompt)
+                     (dumb-jump-get-point-context
+                      (dumb-jump-get-point-line)
+                      look-for
+                      (dumb-jump--get-symbol-start))))
+           (ctx-type
+            (dumb-jump-get-ctx-type-by-language lang pt-ctx))
+
+           (gen-funcs (dumb-jump-pick-grep-variant proj-root))
+           (parse-fn (plist-get gen-funcs :parse))
+           (generate-fn (plist-get gen-funcs :generate))
+           (searcher (plist-get gen-funcs :searcher))
+
+           (regexes (dumb-jump-get-contextual-regexes lang ctx-type searcher))
+
+           (exclude-paths (when config (plist-get config :exclude)))
+           (include-paths (when config (plist-get config :include)))
+                                        ; we will search proj root and all include paths
+           (search-paths (-distinct (-concat (list proj-root) include-paths)))
+                                        ; run command for all
+           (raw-results (--mapcat
+                         ;; TODO: should only pass exclude paths to actual project root
+                         (dumb-jump-run-command look-for it regexes lang exclude-paths cur-file
+                                                cur-line-num parse-fn generate-fn)
+                         search-paths))
+
+           (tramp-path-prefix (or (file-remote-p default-directory) ""))
+
+           (results (delete-dups
+                     (--map
+                      (progn
+                        (plist-put it :target look-for)
+                        (plist-put it :path
+                                   (concat tramp-path-prefix (plist-get it :path))))
+                      raw-results))))
+
+      `(:results ,results :lang ,(if (null lang) "" lang) :symbol ,look-for :ctx-type ,(if (null ctx-type) "" ctx-type) :file ,cur-file :root ,proj-root)))
+
+  )
+
+
+
 
 (provide 'init-dumb-jump)
--- init/lang/init-lua.el
+++ init/lang/init-lua.el
@@ -1,18 +1,39 @@
 
-;; http://stackoverflow.com/questions/4643206/how-to-configure-indentation-in-emacs-lua-mode
+(require 'xref)
+
 
 
 
+;; http://stackoverflow.com/questions/4643206/how-to-configure-indentation-in-emacs-lua-mode
+
 (use-package lua-mode
   :mode "\\.p8$"
+  :bind (("M-." . prf/lua/xref-find-definitions))
+
   :init
   (setq lua-indent-level 2)
+
   :config
+
+  (defun prf/lua/xref-find-definitions (identifier)
+    "Rework of `xref-find-definitions' that replacees generic `thing-at-point' w/ lua's variant (`lua-funcname-at-point')"
+    (interactive (cl-letf (((symbol-function #'xref-backend-identifier-at-point) (lambda (_backend) (lua-funcname-at-point))))
+                   (list (xref--read-identifier "Find definitions of: "))))
+    (xref--find-definitions identifier nil))
+
+  ;; PICO-8
   (defun p8-hook()
     (when (and (buffer-file-name)
-	       (equal (file-name-extension (buffer-file-name)) "p8"))
+	           (equal (file-name-extension (buffer-file-name)) "p8"))
       (setq-local lua-indent-level 1)))
   (add-hook 'lua-mode-hook #'p8-hook))
+
+
+
+;; FENNEL
+
+(use-package fennel-mode
+  :mode "\\.fnl\\'")
 
 
 
@@ -24,10 +45,19 @@
 
 
 
-;; FENNEL
+;; CUSTOM FNS - BYTES
 
-(use-package fennel-mode
-  :mode "\\.fnl\\'")
+(defun prf/hex-str-to-lua (str)
+  (->> (s-split " " str)
+       (--map (concat "0x" (s-downcase it)))
+       (s-join ", ")))
+
+(defun prf/lua/hex-to-table (start end)
+  (interactive "r")
+  (let ((lua-table (prf/hex-str-to-lua (buffer-substring start end))))
+    (kill-region start end)
+    (insert lua-table)))
+
 
 
 
