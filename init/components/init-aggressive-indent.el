8c849c792ad6c3c69f6aaf15ecebb8a47d5c3270
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-09-18 13:16:42 +0200

    fix: init-aggressive-indent attempted setting unbound list

--- init/components/init-aggressive-indent.el
+++ init/components/init-aggressive-indent.el
@@ -6,7 +6,7 @@
                  (when (boundp 'aggressive-indent-excluded-modes)
                    (unless (cl-member-if #'derived-mode-p aggressive-indent-excluded-modes)
                      aggressive-indent-mode))))
-  :init
+  :config
   (add-to-list 'aggressive-indent-excluded-modes 'sql-mode)
   (add-to-list 'aggressive-indent-excluded-modes 'sass-mode)
   (add-to-list 'aggressive-indent-excluded-modes 'dotenv-mode)
