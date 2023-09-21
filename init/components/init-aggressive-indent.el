fd6f85507d811a941be8f5fd663af51c66e67fff
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-09-21 14:24:56 +0200

    fix activation of aggressive-indent

--- init/components/init-aggressive-indent.el
+++ init/components/init-aggressive-indent.el
@@ -1,17 +1,20 @@
 
 (use-package aggressive-indent
   :hook
-  (prog-mode . (lambda ()
-                 ;; NB: `aggressive-indent-excluded-modes' only eval'ed when using `global-aggressive-indent-mode'
-                 (when (boundp 'aggressive-indent-excluded-modes)
-                   (unless (cl-member-if #'derived-mode-p aggressive-indent-excluded-modes)
-                     aggressive-indent-mode))))
-  :config
-  (add-to-list 'aggressive-indent-excluded-modes 'sql-mode)
-  (add-to-list 'aggressive-indent-excluded-modes 'sass-mode)
-  (add-to-list 'aggressive-indent-excluded-modes 'dotenv-mode)
-  (add-to-list 'aggressive-indent-excluded-modes 'dockerfile-mode)
-  (add-to-list 'aggressive-indent-excluded-modes 'makefile-mode))
+  (prog-mode . #'prog-mode-hook-aggressive-indent)
+  :init
+  ;; NB: `aggressive-indent-excluded-modes' exists but doesn't get bound untill calling `aggressive-indent-mode' once or using `global-aggressive-indent-mode'...
+  (defvar prf/aggressive-indent-excluded-modes
+    '(
+      ;; `aggressive-indent-excluded-modes' defautl value
+      elm-mode haskell-mode inf-ruby-mode makefile-mode makefile-gmake-mode python-mode sql-interactive-mode text-mode yaml-mode
+
+      ;; added by me
+      dockerfile-mode dotenv-mode sass-mode sql-mode
+      ))
+  (defun prog-mode-hook-aggressive-indent ()
+    (unless (cl-member-if #'derived-mode-p prf/aggressive-indent-excluded-modes)
+      aggressive-indent-mode)))
 
 
 
