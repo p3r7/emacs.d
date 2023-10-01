8d0607204267cdeffe02cb3278a287accc9441c9
p3r7 <11557146+p3r7@users.noreply.github.com>
2023-10-01 10:28:20 +0200

    more shortcuts

--- init/components/init-norns.el
+++ init/components/init-norns.el
@@ -17,7 +17,14 @@
          ("C-c e b" . norns-load-current-script)
          ("<XF86Open>" . norns-load-current-script)
          ("C-c e r" . norns-send-selection)
-         ("M-<XF86Open>" . norns-send-selection))
+         ("M-<XF86Open>" . norns-send-selection)
+
+         :map norns-maiden-repl-mode-map
+         ("C-c e b" . norns-rerun)
+         ("<XF86Open>" . norns-rerun)
+
+         :map norns-sc-repl-mode-map
+         ("C-." . norns-sc-stop))
   :config
   (add-hook 'lua-mode-hook #'norns-mode-maybe-activate)
   (add-hook 'fennel-mode-hook #'norns-mode-maybe-activate)
