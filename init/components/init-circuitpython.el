b15053497139c0aca951edf7edbce422df1211d0
perf <11557146+p3r7@users.noreply.github.com>
2023-12-01 11:30:14 +0100

    don't hard-code tty device path

--- init/components/init-circuitpython.el
+++ init/components/init-circuitpython.el
@@ -1,19 +1,20 @@
 
+;; NB: could theoretically not have to rely on `vterm' + `screen' using `make-serial-process', but it's kinda hard to make it behave properly
+
 (with-eval-after-load 'vterm-toggle
   (defvar vterm-circuitpython-buff nil)
-
-  (defvar circuitpython-tty "/dev/tty.usbmodem11101")
 
   (defun circuitpython-repl ()
     "Launch circuitpython repl.
 To kill, as it's running under a screen session, use C-a k."
     (interactive)
-
-    (unless (file-exists-p circuitpython-tty)
-      (user-error ""))
-
-    (let ((vterm-toggle-use-dedicated-buffer t)
+    (let ((circuitpython-tty (car (f-glob "/dev/tty.usbmodem*")))
+          (vterm-toggle-use-dedicated-buffer t)
           (vterm-toggle--vterm-dedicated-buffer vterm-circuitpython-buff))
+
+      (unless (file-exists-p circuitpython-tty)
+        (user-error "No circuitpython TTY found!"))
+
       (with-current-buffer (vterm-toggle-cd)
         (setq vterm-circuitpython-buff (current-buffer))
         (rename-buffer "*circuitpython-repl*")
