6ce1d48eb6bf00cf50f851e81b699823580bf42f
p3r7 <11557146+p3r7@users.noreply.github.com>
2025-03-06 11:27:23 +0100

    dirty patch for emacs 30

--- init/components/completion-at-point/init-ac.el
+++ init/components/completion-at-point/init-ac.el
@@ -7,16 +7,23 @@
   :after (auto-complete)
 
   :bind (
-	 :map ac-mode-map
+	     :map ac-mode-map
          ("M-TAB" . auto-complete)
          ;; ("<M-S-iso-lefttab>" . auto-complete)
-	 :map ac-completing-map
-	 ("TAB" . ac-complete)
-	 ("RET" . nil))
+	     :map ac-completing-map
+	     ("TAB" . ac-complete)
+	     ("RET" . nil))
 
   :config
   (when (member prf/fav-completion-at-point '(ac auto-complete))
-    (ac-config-default)))
+    (ac-config-default))
+
+  ;; see https://github.com/auto-complete/auto-complete/issues/533
+  ;; https://lists.gnu.org/archive/html/bug-gnu-emacs/2025-02/msg02204.html
+  (when (= emacs-major-version 30)
+    (add-hook 'auto-complete-mode-hook
+              (lambda ()
+                (setq ac-sources (remove 'ac-source-abbrev ac-sources))))))
 
 
 
