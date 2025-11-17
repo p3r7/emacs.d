453d69181a07761e5961d88af2d376002b18aa31
p3r7 <11557146+p3r7@users.noreply.github.com>
2025-11-17 12:09:11 +0100

    js most std indent level is 2

--- init/lang/init-js.el
+++ init/lang/init-js.el
@@ -5,13 +5,16 @@
 (use-package js2-mode
   :delight (js2-mode "JS2")
   :mode "\\.js\\'"
+
+  :init
+  (setq js-indent-level 2)
+
   :config
   ;; (customize-set-variable 'js2-include-node-externs t)
 
-
   (defun max-msp-js-hook ()
     (when (and (buffer-file-name)
-	       (s-contains? "/Max for Live Devices/"  (buffer-file-name)))
+	           (s-contains? "/Max for Live Devices/"  (buffer-file-name)))
       (setq indent-tabs-mode t
             tab-width 4
             ;; standard-indent 4
@@ -19,20 +22,20 @@
   (add-hook 'js2-mode-hook #'max-msp-js-hook))
 
 (add-hook 'js3-mode-hook
-	  (lambda ()
-	    (setq js3-auto-indent-p t
-		  js3-indent-level 4
-		  js3-expr-indent-offset 4
-		  js3-paren-indent-offset 2
-		  js3-square-indent-offset 4
-		  js3-curly-indent-offset 0
-		  js3-enter-indents-newline t
-		  js3-indent-on-enter-key t
-		  js3-lazy-commas t
-		  js3-lazy-dots t
-		  js3-lazy-operators t
-		  )
-	    (linum-mode 1)))
+	      (lambda ()
+	        (setq js3-auto-indent-p t
+		          js3-indent-level 2
+		          js3-expr-indent-offset 4
+		          js3-paren-indent-offset 2
+		          js3-square-indent-offset 4
+		          js3-curly-indent-offset 0
+		          js3-enter-indents-newline t
+		          js3-indent-on-enter-key t
+		          js3-lazy-commas t
+		          js3-lazy-dots t
+		          js3-lazy-operators t
+		          )
+	        (linum-mode 1)))
 ;; (add-to-list 'ac-modes 'js3-mode)
 
 
