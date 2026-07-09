c1b77dfda89c0c210b49af509e82a87f789a54e2
p3r7 <11557146+p3r7@users.noreply.github.com>
2026-07-09 10:36:18 +0200

    enable profile on startup

--- init.el
+++ init.el
@@ -8,8 +8,11 @@
 
 ;; OPTIMIZATIONS
 
+(profiler-start 'cpu)
+
 (add-hook 'emacs-startup-hook
           (lambda ()
+            (profiler-stop)
             (message "Emacs ready in %s with %d garbage collections."
                      (format "%.2f seconds"
                              (float-time
